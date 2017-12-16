d4aaae9b771878d43c905d484add72404dae22af
Adam Shaw <arshaw@users.noreply.github.com>
2017-12-16 00:35:48 -0500

    use simulate test util from core

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,7 @@
 require('fullcalendar/tests/globals')
 require('fullcalendar/tests/lib/moment')
 require('fullcalendar/tests/lib/dom-geom')
+require('fullcalendar/tests/lib/simulate')
 
 var context = require.context(
   '.',
