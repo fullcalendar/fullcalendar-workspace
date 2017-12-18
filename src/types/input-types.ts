72888f45078755c8e2c213d52fff7b874861cc53
Adam Shaw <arshaw@users.noreply.github.com>
2017-12-17 22:54:34 -0500

    add more methods and settings to typedef

--- src/types/input-types.ts
+++ src/types/input-types.ts
@@ -1,22 +1,9 @@
-import { BusinessHoursInput } from 'fullcalendar'
-
-declare module 'fullcalendar/src/types/input-types' {
-
-  interface DropInfo {
-    resourceId?: string
-  }
-
-  interface OptionsInputBase {
-    resourceAreaWidth?: number
-    schedulerLicenseKey?: string
-    resourceLabelText?: any
-    resourceColumns?: any
-  }
-
-}
+import * as moment from 'moment'
+import { BusinessHoursInput, EventOptionsBase } from 'fullcalendar'
 
 export interface ResourceInput {
   id?: string
+  title?: string
   eventColor?: string
   eventBackgroundColor?: string
   eventBorderColor?: string
@@ -26,4 +13,38 @@ export interface ResourceInput {
   children?: ResourceInput[]
   parentId?: string
   parent?: ResourceInput
+}
+
+export interface ResourceComplexInput extends EventOptionsBase, JQueryAjaxSettings {
+}
+
+export type ResourceFunctionCallback = (resources: ResourceInput[]) => void
+export type ResourceFunction = (callback: ResourceFunctionCallback, start: moment.Moment, end: moment.Moment, timezone: string) => void
+export type ResourceSourceInput = ResourceInput[] | ResourceFunction | ResourceComplexInput
+
+declare module 'fullcalendar/src/types/input-types' {
+
+  interface DropInfo {
+    resourceId?: string
+  }
+
+  interface OptionsInputBase {
+    schedulerLicenseKey?: string
+    resourceAreaWidth?: number
+    resourceLabelText?: string
+    resourceColumns?: any
+    resources?: ResourceSourceInput
+    refetchResourcesOnNavigate?: boolean
+    groupByResource?: boolean
+    groupByDateAndResource?: boolean,
+    resourceOrder?: string
+    resourceGroupField?: string
+    resourceGroupText?: (groupValue: string) => string
+    resourcesInitiallyExpanded?: boolean
+    filterResourcesWithEvents?: boolean
+    resourceText?: (resource: ResourceInput) => string
+    resourceRender?: (resource: ResourceInput, labelTds, bodyTds) => void
+    eventResourceEditable?: boolean
+  }
+
 }
