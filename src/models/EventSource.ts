df17d586cc0cdc10c4db159fec92fac82cce9a87
jboulay <jboulay@ekito.fr>
2018-02-04 19:13:41 +0100

    Extract resourceId from eventSource if it exists

--- src/models/EventSource.ts
+++ src/models/EventSource.ts
@@ -2,4 +2,5 @@ import { EventSource } from 'fullcalendar'
 
 // defineStandardProps won't work :(
 // TODO: find a better way
-(EventSource.prototype as any).standardPropMap.resourceEditable = true // automatically transfer
+(EventSource.prototype as any).standardPropMap.resourceEditable = true;  // automatically transfer
+(EventSource.prototype as any).standardPropMap.resourceId = true
