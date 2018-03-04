44ca37d0d06c67abee5abe07718ffefd8319fb4b
Adam Shaw <arshaw@users.noreply.github.com>
2018-03-04 17:47:52 -0500

    export types: ResourceInput, ResourceSourceInput

--- src/exports.ts
+++ src/exports.ts
@@ -5,6 +5,11 @@ import ResourceMonthView from './resource-basic/ResourceMonthView'
 import TimelineView from './timeline/TimelineView'
 import ResourceTimelineView from './resource-timeline/ResourceTimelineView'
 
+export {
+  ResourceInput,
+  ResourceSourceInput
+} from './types/input-types'
+
 // TODO: find a better way
 (exportHooks as any).ResourceAgendaView = ResourceAgendaView;
 (exportHooks as any).ResourceBasicView = ResourceBasicView;
