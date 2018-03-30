ad770cd6b4937ff57757480ba25a5c1744905de7
Adam Shaw <arshaw@users.noreply.github.com>
2018-03-30 17:12:42 -0400

    forgot superagent for karma

--- karma.config.js
+++ karma.config.js
@@ -9,6 +9,7 @@ module.exports = function(config) {
     files: [
       // dependencies for main lib
       'node_modules/moment/moment.js',
+      'node_modules/superagent/superagent.js',
       'node_modules/jquery/dist/jquery.js',
       'node_modules/components-jqueryui/jquery-ui.js',
       'node_modules/components-jqueryui/themes/cupertino/jquery-ui.css',
