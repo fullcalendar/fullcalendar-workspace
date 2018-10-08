fd771bc65103b7521b0d9e6381dcc62a1c5219d7
Adam Shaw <arshaw@users.noreply.github.com>
2018-10-08 13:05:27 -0400

    render timeline event els

--- src2/timeline/HEventLane.ts
+++ src2/timeline/HEventLane.ts
@@ -1,4 +1,4 @@
-import { DateComponent, DateComponentRenderState, RenderForceFlags, Seg, DateRange, EventRenderer, intersectRanges, addMs } from 'fullcalendar'
+import { DateComponent, DateComponentRenderState, RenderForceFlags, Seg, DateRange, EventRenderer, intersectRanges, addMs, htmlEscape, cssToStr } from 'fullcalendar'
 import { TimelineDateProfile, normalizeRange, isValidDate } from './timeline-date-profile'
 import TimelineView from './TimelineView'
 
@@ -35,7 +35,6 @@ export default class HEventLane extends DateComponent {
       }
     }
 
-    console.log(segs)
     return segs
   }
 
@@ -43,6 +42,50 @@ export default class HEventLane extends DateComponent {
 
 class HEventLaneEventRenderer extends EventRenderer {
 
+  renderFgSegs(segs: Seg[]) {
+    console.log(segs)
+  }
+
+  fgSegHtml(seg) {
+    let eventRange = seg.eventRange
+    let eventDef = eventRange.def
+    let eventUi = eventRange.ui
+    let isDraggable = eventUi.startEditable
+    let isResizableFromStart = seg.isStart && eventUi.durationEditable && this.opt('eventResizableFromStart')
+    let isResizableFromEnd = seg.isEnd && eventUi.durationEditable
+
+    const classes = this.getSegClasses(seg, isDraggable, isResizableFromStart || isResizableFromEnd)
+    classes.unshift('fc-timeline-event', 'fc-h-event')
+
+    const timeText = this.getTimeText(eventRange)
+
+    return '<a class="' + classes.join(' ') + '" style="' + cssToStr(this.getSkinCss(eventUi)) + '"' +
+      (eventDef.url ?
+        ' href="' + htmlEscape(eventDef.url) + '"' :
+        '') +
+      '>' +
+      '<div class="fc-content">' +
+        (timeText ?
+          '<span class="fc-time">' +
+            htmlEscape(timeText) +
+          '</span>'
+        :
+          '') +
+        '<span class="fc-title">' +
+          (eventDef.title ? htmlEscape(eventDef.title) : '&nbsp;') +
+        '</span>' +
+      '</div>' +
+      '<div class="fc-bg"></div>' +
+      (isResizableFromStart ?
+        '<div class="fc-resizer fc-start-resizer"></div>' :
+        '') +
+      (isResizableFromEnd ?
+        '<div class="fc-resizer fc-end-resizer"></div>' :
+        '') +
+    '</a>'
+  }
+
 }
 
 HEventLane.prototype.eventRendererClass = HEventLaneEventRenderer
+// TODO: fill rendering
