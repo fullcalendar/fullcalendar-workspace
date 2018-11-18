825e96e84a59b2dc93219143356b7bacab811f15
Adam Shaw <arshaw@users.noreply.github.com>
2018-11-18 18:49:12 -0500

    businessHours for ResourceDayGrid

--- src2/resource-agenda/ResourceDayGrid.ts
+++ src2/resource-agenda/ResourceDayGrid.ts
@@ -1,4 +1,4 @@
-import { sliceDayGridSegs, DateSpan, DayGrid, Component, assignTo, DateProfile, EventStore, EventUiHash, EventInteractionUiState, ComponentContext, DayGridSeg, Duration, DateRange } from "fullcalendar"
+import { sliceDayGridSegs, DateSpan, DayGrid, Component, assignTo, DateProfile, EventStore, EventUiHash, EventInteractionUiState, ComponentContext, DayGridSeg, Duration, DateRange, sliceBusinessHours, reselector } from "fullcalendar"
 import { AbstractResourceDayTable } from './resource-day-table'
 import { ResourceAwareSlicer } from './resource-aware-slicing'
 
@@ -21,6 +21,7 @@ export default class ResourceDayGrid extends Component<ResourceDayGridProps> {
   dayGrid: DayGrid
 
   private slicer = new ResourceAwareSlicer(sliceSegs)
+  private sliceResourceBusinessHours = reselector(sliceResourceBusinessHours)
 
   constructor(context: ComponentContext, dayGrid: DayGrid) {
     super(context)
@@ -36,7 +37,7 @@ export default class ResourceDayGrid extends Component<ResourceDayGridProps> {
     this.dayGrid.receiveProps({
       dateProfile: props.dateProfile,
       cells: props.resourceDayTable.cells,
-      businessHourSegs: slicer.businessHoursToSegs(props.businessHours, dateProfile, nextDayThreshold, resourceDayTable, isRtl),
+      businessHourSegs: this.sliceResourceBusinessHours(resourceDayTable, dateProfile, nextDayThreshold, props.businessHours, this.dayGrid),
       eventSegs: slicer.eventStoreToSegs(props.eventStore, props.eventUis, dateProfile, nextDayThreshold, resourceDayTable, isRtl),
       dateSelectionSegs: slicer.selectionToSegs(props.dateSelection, resourceDayTable, isRtl),
       eventSelection: props.eventSelection,
@@ -47,6 +48,7 @@ export default class ResourceDayGrid extends Component<ResourceDayGridProps> {
   }
 
 }
+
 
 function sliceSegs(range: DateRange, resourceIds: string[], resourceDayTable: AbstractResourceDayTable, isRtl: boolean): DayGridSeg[] {
 
@@ -65,6 +67,40 @@ function sliceSegs(range: DateRange, resourceIds: string[], resourceDayTable: Ab
       if (resourceI != null) {
         segs.push(
           assignTo({}, rawSeg, {
+            leftCol: resourceDayTable.computeCol(rawSeg.leftCol, resourceI),
+            rightCol: resourceDayTable.computeCol(rawSeg.rightCol, resourceI)
+          })
+        )
+      }
+    }
+  }
+
+  return segs
+}
+
+
+function sliceResourceBusinessHours(resourceDayTable: AbstractResourceDayTable, dateProfile: DateProfile, nextDayThreshold: Duration, fallbackBusinessHours: EventStore, component: DayGrid) {
+  let segs = []
+
+  for (let resource of resourceDayTable.resources) {
+    let businessHours = resource.businessHours || fallbackBusinessHours
+    let eventRanges = sliceBusinessHours(
+      businessHours,
+      dateProfile.activeRange,
+      nextDayThreshold,
+      component.calendar
+    )
+
+    let resourceI = resourceDayTable.resourceIndex.indicedByInternalId[resource.resourceId]
+
+    for (let eventRange of eventRanges) {
+      let rawSegs = sliceDayGridSegs(eventRange.range, resourceDayTable.dayTable, component.isRtl)
+
+      for (let rawSeg of rawSegs) {
+        segs.push(
+          assignTo({}, rawSeg, {
+            component,
+            eventRange,
             leftCol: resourceDayTable.computeCol(rawSeg.leftCol, resourceI),
             rightCol: resourceDayTable.computeCol(rawSeg.rightCol, resourceI)
           })
