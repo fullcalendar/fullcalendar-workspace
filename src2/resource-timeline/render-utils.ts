5a697226c4d12bf07b3d60253ec97afc87288860
Adam Shaw <arshaw@users.noreply.github.com>
2018-11-21 16:50:04 -0500

    forgot icon utils

---
+++ src2/resource-timeline/render-utils.ts
@@ -0,0 +1,25 @@
+
+const LEFT_TRIANGLE_ICON = 'fc-icon-left-triangle'
+const RIGHT_TRIANGLE_ICON = 'fc-icon-right-triangle'
+const DOWN_TRIANGLE_ICON = 'fc-icon-down-triangle'
+
+export function updateExpanderIcon(el: HTMLElement, isExpanded: boolean, isRtl: boolean) {
+  let { classList } = el
+
+  if (isExpanded) {
+    classList.remove(LEFT_TRIANGLE_ICON)
+    classList.remove(RIGHT_TRIANGLE_ICON)
+    classList.add(DOWN_TRIANGLE_ICON)
+  } else {
+    classList.remove(DOWN_TRIANGLE_ICON)
+    classList.add(this.isRtl ? LEFT_TRIANGLE_ICON : RIGHT_TRIANGLE_ICON)
+  }
+}
+
+export function clearExpanderIcon(el: HTMLElement) {
+  let { classList } = el
+
+  classList.remove(LEFT_TRIANGLE_ICON)
+  classList.remove(RIGHT_TRIANGLE_ICON)
+  classList.remove(DOWN_TRIANGLE_ICON)
+}
