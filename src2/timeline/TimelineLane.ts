e95ffebacca974e5afe68f63954260df0b4a3a9e
Adam Shaw <arshaw@users.noreply.github.com>
2018-11-23 22:00:50 -0500

    move TimelineLane to new stuff

--- src2/timeline/TimelineLane.ts
+++ src2/timeline/TimelineLane.ts
@@ -1,4 +1,4 @@
-import { EventStore, EventUiHash, DateMarker, DateSpan, EventRenderRange, sliceBusinessHours, sliceEventStore, EventInteractionUiState, DateComponent, ComponentContext, Seg, DateRange, intersectRanges, addMs, DateProfile, memoizeRendering } from 'fullcalendar'
+import { EventStore, EventUiHash, DateMarker, DateSpan, MemoizedRendering, EventInteractionUiState, EventSegUiInteractionState, DateComponent, ComponentContext, Seg, DateRange, intersectRanges, addMs, DateProfile, memoizeRendering, Slicer, assignTo } from 'fullcalendar'
 import { TimelineDateProfile, normalizeRange, isValidDate } from './timeline-date-profile'
 import TimelineLaneEventRenderer from './TimelineLaneEventRenderer'
 import TimelineLaneFillRenderer from './TimelineLaneFillRenderer'
@@ -25,78 +25,98 @@ export default class TimelineLane extends DateComponent<TimelineLaneProps> {
   tDateProfile: TimelineDateProfile
   timeAxis: TimeAxis
 
-  private _renderEvents = memoizeRendering(this.renderEvents, this.unrenderEvents)
-  private _renderBusinessHours = memoizeRendering(this.renderBusinessHours, this.unrenderBusinessHours)
-  private _renderDateSelection = memoizeRendering(this.renderDateSelection, this.unrenderDateSelection)
-  private _renderEventDragState = memoizeRendering(this.renderEventDragState, this.unrenderEventDragState)
-  private _renderEventResizeState = memoizeRendering(this.renderEventResizeState, this.unrenderEventResizeState)
+  private slicer = new Slicer(rangeToSegs)
+  private renderBusinessHours: MemoizedRendering<[TimelineLaneSeg[]]>
+  private renderDateSelection: MemoizedRendering<[TimelineLaneSeg[]]>
+  private renderEvents = memoizeRendering(this._renderEventSegs, this._unrenderEventSegs)
+  private renderEventSelection: MemoizedRendering<[string]>
+  private renderEventDrag = memoizeRendering(this._renderEventDrag, this._unrenderEventDrag)
+  private renderEventResize = memoizeRendering(this._renderEventResize, this._unrenderEventResize)
+
 
   constructor(context: ComponentContext, fgContainerEl: HTMLElement, bgContainerEl: HTMLElement, timeAxis: TimeAxis) {
     super(context, bgContainerEl) // should el be bgContainerEl???
 
-    this.fillRenderer = new TimelineLaneFillRenderer(context, bgContainerEl, timeAxis)
-    this.eventRenderer = new TimelineLaneEventRenderer(context, fgContainerEl, timeAxis)
+    this.slicer.component = this.view // ? (just like below)
+
+    let fillRenderer = this.fillRenderer = new TimelineLaneFillRenderer(context, bgContainerEl, timeAxis)
+    let eventRenderer = this.eventRenderer = new TimelineLaneEventRenderer(context, fgContainerEl, timeAxis)
     this.mirrorRenderer = new TimelineLaneEventRenderer(context, fgContainerEl, timeAxis)
+
+    this.renderBusinessHours = memoizeRendering(
+      fillRenderer.renderSegs.bind(fillRenderer, 'businessHours'),
+      fillRenderer.unrender.bind(fillRenderer, 'businessHours')
+    )
+
+    this.renderDateSelection = memoizeRendering(
+      fillRenderer.renderSegs.bind(fillRenderer, 'highlight'),
+      fillRenderer.unrender.bind(fillRenderer, 'highlight')
+    )
+
+    this.renderEventSelection = memoizeRendering(
+      eventRenderer.selectByInstanceId.bind(eventRenderer),
+      eventRenderer.unselectByInstanceId.bind(eventRenderer),
+      [ this.renderEvents ]
+    )
 
     this.timeAxis = timeAxis
   }
 
   render(props: TimelineLaneProps) {
-    this._renderEvents(props.eventStore, props.eventUis)
-    this._renderBusinessHours(props.businessHours, props.dateProfile)
-    this._renderDateSelection(props.dateSelection)
-    this._renderEventDragState(props.eventDrag)
-    this._renderEventResizeState(props.eventResize)
+    let { slicer, timeAxis } = this
+    let { dateProfile } = props
+
+    this.renderEvents(slicer.eventStoreToSegs(props.eventStore, props.eventUis, dateProfile, null, timeAxis))
+    this.renderEventSelection(props.eventSelection)
+    this.renderBusinessHours(slicer.businessHoursToSegs(props.businessHours, dateProfile, null, timeAxis))
+    this.renderDateSelection(slicer.selectionToSegs(props.dateSelection, timeAxis))
+    this.renderEventDrag(slicer.buildEventDrag(props.eventDrag, dateProfile, null, timeAxis))
+    this.renderEventResize(slicer.buildEventResize(props.eventResize, dateProfile, null, timeAxis))
   }
 
-  renderEvents(eventStore: EventStore, eventUis: EventUiHash) {
-    if (eventStore) {
-      this.renderEventSegs(
-        eventStoreToSegs(eventStore, eventUis, this.timeAxis)
-      )
-    }
+  destroy() {
+    super.destroy()
+
+    this.renderEvents.unrender()
+    this.renderEventSelection.unrender()
+    this.renderBusinessHours.unrender()
+    this.renderDateSelection.unrender()
+    this.renderEventDrag.unrender()
+    this.renderEventResize.unrender()
   }
 
-  renderBusinessHours(businessHours: EventStore, dateProfile: DateProfile) {
-    if (businessHours) {
-      this.renderBusinessHourSegs(
-        eventRangesToSegs(
-          sliceBusinessHours(businessHours, dateProfile.activeRange, null, this.calendar),
-          this.timeAxis
-        )
-      )
-    }
-  }
-
-  renderDateSelection(selection: DateSpan) {
-    if (selection) {
-      this.renderDateSelectionSegs(
-        rangeToSegs(selection.range, this.timeAxis)
-      )
-    }
-  }
-
-  renderEventDragState(state: EventInteractionUiState) {
+  _renderEventDrag(state: EventSegUiInteractionState) {
     if (state) {
-      this.renderEventDragSegs(buildSegUiState(state, this.timeAxis))
+      this.eventRenderer.hideByHash(state.affectedInstances)
+      this.mirrorRenderer.renderSegs(state.segs, { isDragging: true, sourceSeg: state.sourceSeg })
     }
   }
 
-  unrenderEventDragState(state: EventInteractionUiState) {
+  _unrenderEventDrag(state: EventSegUiInteractionState) {
     if (state) {
-      this.unrenderEventDrag(state.affectedEvents.instances)
+      this.eventRenderer.showByHash(state.affectedInstances)
+      this.mirrorRenderer.unrender()
     }
   }
 
-  renderEventResizeState(state: EventInteractionUiState) {
+  _renderEventResize(state: EventSegUiInteractionState) {
     if (state) {
-      this.renderEventResizeSegs(buildSegUiState(state, this.timeAxis))
+      // HACK. eventRenderer and fillRenderer both use these segs. would compete over seg.el
+      let segsForHighlight = state.segs.map(function(seg) {
+        return assignTo({}, seg)
+      })
+
+      this.eventRenderer.hideByHash(state.affectedInstances)
+      this.fillRenderer.renderSegs('highlight', segsForHighlight)
+      this.mirrorRenderer.renderSegs(state.segs, { isDragging: true, sourceSeg: state.sourceSeg })
     }
   }
 
-  unrenderEventResizeState(state: EventInteractionUiState) {
+  _unrenderEventResize(state: EventSegUiInteractionState) {
     if (state) {
-      this.unrenderEventDrag(state.affectedEvents.instances)
+      this.eventRenderer.showByHash(state.affectedInstances)
+      this.fillRenderer.unrender('highlight')
+      this.mirrorRenderer.unrender()
     }
   }
 
@@ -112,38 +132,6 @@ export default class TimelineLane extends DateComponent<TimelineLaneProps> {
     mirrorRenderer.assignSizes(isResize)
   }
 
-}
-
-TimelineLane.prototype.doesDragMirror = true
-
-
-function eventStoreToSegs(eventStore: EventStore, eventUis: EventUiHash, timeAxis: TimeAxis): TimelineLaneSeg[] {
-  return eventRangesToSegs(
-    sliceEventStore(eventStore, eventUis, timeAxis.props.dateProfile.activeRange),
-    timeAxis
-  )
-}
-
-function eventRangesToSegs(eventRanges: EventRenderRange[], timeAxis: TimeAxis): TimelineLaneSeg[] {
-  let segs = []
-
-  for (let eventRange of eventRanges) {
-    segs.push(...eventRangeToSegs(eventRange, timeAxis))
-  }
-
-  return segs
-}
-
-function eventRangeToSegs(eventRange: EventRenderRange, timeAxis: TimeAxis): TimelineLaneSeg[] {
-  let segs = rangeToSegs(eventRange.range, timeAxis)
-
-  for (let seg of segs) {
-    seg.eventRange = eventRange
-    seg.isStart = seg.isStart && eventRange.isStart
-    seg.isEnd = seg.isEnd && eventRange.isEnd
-  }
-
-  return segs
 }
 
 function rangeToSegs(origRange: DateRange, timeAxis: TimeAxis): TimelineLaneSeg[] {
@@ -170,13 +158,4 @@ function rangeToSegs(origRange: DateRange, timeAxis: TimeAxis): TimelineLaneSeg[
   }
 
   return segs
-}
-
-function buildSegUiState(state: EventInteractionUiState, timeAxis: TimeAxis) {
-  return {
-    segs: eventStoreToSegs(state.mutatedEvents, state.eventUis, timeAxis),
-    affectedInstances: state.affectedEvents.instances,
-    isEvent: state.isEvent,
-    sourceSeg: state.origSeg
-  }
 }
