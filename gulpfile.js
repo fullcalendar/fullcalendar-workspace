b11b93bf20ae50fab29f3ee49d1176e492794e57
Adam Shaw <arshaw@users.noreply.github.com>
2019-01-23 19:46:30 -0300

    build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,5 @@ const del = require('del')
 const gulp = require('gulp')
 
 // require('./tasks/archive')
+require('./tasks/build')
 require('./tasks/dts')
---
+++ tasks/build.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const gulp = require('gulp')
+const shell = require('gulp-shell')
+const modify = require('gulp-modify-file')
+const rootPackageConfig = require('../package.json')
+
+gulp.task('build', [ 'build:raw' ], function() {
+  return gulp.src('dist/*/*.{js,css}', { base: '.' })
+    .pipe(
+      modify(modifySource)
+    )
+    .pipe(
+      gulp.dest('.')
+    )
+})
+
+gulp.task('build:raw', shell.task(
+  'npm run build'
+))
+
+const BANNER =
+  '/*!\n' +
+  '<%= name %> v<%= version %>\n' +
+  'Docs & License: <%= homepage %>\n' +
+  '(c) <%= copyright %>\n' +
+  '*/\n'
+
+function modifySource(content, filePath) {
+  let packageName = path.basename(path.dirname(filePath))
+  let vars = Object.assign(rootPackageConfig, {
+    name: packageName
+  })
+
+  content = BANNER + content
+
+  content = content.replace(
+    /<%=\s*(\w+)\s*%>/g,
+    function(wholeMatch, varName) {
+      return vars[varName] || ''
+    }
+  )
+
+  return content
+}
