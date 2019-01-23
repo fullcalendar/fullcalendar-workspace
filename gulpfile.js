4a11314bf0ad2400923688d115e0c8d8e5f72487
Adam Shaw <arshaw@users.noreply.github.com>
2019-01-23 20:02:02 -0300

    archive

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
+const gulp = require('gulp')
 
-// require('./tasks/archive')
 require('./tasks/build')
 require('./tasks/dts')
+// require('./tasks/package-meta')
 require('./tasks/lint')
+require('./tasks/archive')
+
+gulp.task('dist', [
+  'build',
+  'dts',
+  // 'package-meta'
+])
--- tasks/archive.js
+++ tasks/archive.js
@@ -1,64 +1,117 @@
 const gulp = require('gulp')
-const rename = require('gulp-rename')
 const filter = require('gulp-filter')
 const modify = require('gulp-modify-file')
+const uglify = require('gulp-uglify')
+const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
 const zip = require('gulp-zip')
 
 // determines the name of the ZIP file
-const packageConf = require('../package.json')
-const packageId = packageConf.name + '-' + packageConf.version
+const packageConfig = require('../package.json')
+const archiveId = packageConfig.name + '-' + (packageConfig.version || 'latest')
 
-gulp.task('archive', [
-  'archive:dist',
-  'archive:misc',
-  'archive:deps',
-  'archive:demos'
-], function() {
+// assumes a clean dist already happened
+gulp.task('archive', [ 'archive:files' ], function() {
   // make the zip, with a single root directory of a similar name
-  return gulp.src(`tmp/${packageId}/**/*`, { base: 'tmp/' })
-    .pipe(zip(packageId + '.zip'))
-    .pipe(gulp.dest('dist/'))
+  return gulp.src('tmp/' + archiveId + '/**', { base: 'tmp/' })
+    .pipe(
+      zip(archiveId + '.zip')
+    )
+    .pipe(
+      gulp.dest('archives/')
+    )
 })
 
-gulp.task('archive:dist', [ 'dist' ], function() {
-  return gulp.src('dist/*.{js,css}') // matches unminified and minified files
-    .pipe(gulp.dest(`tmp/${packageId}/`))
+gulp.task('archive:files', [
+  'archive:corepackages',
+  'archive:packages',
+  'archive:packages:minjs',
+  'archive:packages:mincss',
+  'archive:demos',
+  'archive:vendor',
+  'archive:meta'
+])
+
+gulp.task('archive:corepackages', [ 'build' ], function() {
+  return gulp.src([
+    'fullcalendar/dist/**/*.{js,css}'
+  ]).pipe(
+    gulp.dest('tmp/' + archiveId + '/packages')
+  )
 })
 
-gulp.task('archive:misc', function() {
+gulp.task('archive:packages', [ 'build' ], function() {
+  return gulp.src([
+    'dist/**/*.{js,css}'
+  ]).pipe(
+    gulp.dest('tmp/' + archiveId + '/packages')
+  )
+})
+
+gulp.task('archive:packages:minjs', [ 'archive:packages' ], function() {
+  return gulp.src([
+    'tmp/' + archiveId + '/packages/*/*.js',
+    '!**/*.min.js' // avoid double minify
+  ], { base: '.' })
+    .pipe(
+      uglify({
+        preserveComments: 'some' // keep comments starting with !
+      })
+    )
+    .pipe(
+      rename({ extname: '.min.js' })
+    )
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('archive:packages:mincss', [ 'archive:packages' ], function() {
+  return gulp.src([
+    'tmp/' + archiveId + '/packages/*/*.css',
+    '!**/*.min.css' // avoid double minify
+  ], { base: '.' })
+    .pipe(
+      cssmin()
+    )
+    .pipe(
+      rename({ extname: '.min.css' })
+    )
+    .pipe(
+      gulp.dest('.')
+    )
+})
+
+gulp.task('archive:meta', function() {
   return gulp.src([
     'LICENSE.*',
-    'CHANGELOG.*',
-    'CONTRIBUTING.*'
-  ])
-    .pipe(rename({ extname: '.txt' }))
-    .pipe(gulp.dest(`tmp/${packageId}/`))
+    'CHANGELOG.*'
+  ]).pipe(
+    gulp.dest('tmp/' + archiveId)
+  )
 })
 
-gulp.task('archive:deps', function() {
-  return gulp.src([
-    'node_modules/moment/min/moment.min.js',
-    'node_modules/superagent/superagent.js',
-    'node_modules/jquery/dist/jquery.min.js', // only for draggable example
-    'node_modules/components-jqueryui/jquery-ui.min.js', // "
-    'fullcalendar/dist/fullcalendar.min.js',
-    'fullcalendar/dist/fullcalendar.min.css',
-    'fullcalendar/dist/plugins/gcal.min.js',
-    'fullcalendar/dist/jquery-ui-draggable.min.js'
-  ])
-    .pipe(gulp.dest(`tmp/${packageId}/lib/`))
-})
-
-// transfers demo files, transforming their paths to dependencies
 gulp.task('archive:demos', function() {
-  return gulp.src('**/*', { cwd: 'demos/', base: 'demos/' })
+  return gulp.src([
+    'demos/**',
+    '!**/_*' // no files that start with underscore
+  ])
     .pipe(htmlFileFilter)
     .pipe(demoPathModify)
     .pipe(htmlFileFilter.restore) // pipe thru files that didn't match htmlFileFilter
-    .pipe(gulp.dest(`tmp/${packageId}/demos/`))
+    .pipe(
+      gulp.dest('tmp/' + archiveId + '/demos')
+    )
 })
 
-const htmlFileFilter = filter('*.html', { restore: true })
+gulp.task('archive:vendor', [ 'archive:demos' ], function() {
+  return gulp.src(
+    vendorPaths,
+    { cwd: 'node_modules' } // a cwd without a base will flatten the paths. what we want
+  ).pipe(
+    gulp.dest('tmp/' + archiveId + '/packages')
+  )
+})
+
+const htmlFileFilter = filter('**/*.html', { restore: true })
 const demoPathModify = modify(function(content) {
   return content.replace(
     /((?:src|href)=['"])([^'"]*)(['"])/g,
@@ -68,25 +121,19 @@ const demoPathModify = modify(function(content) {
   )
 })
 
-const transformDemoPath = function(path) {
-  // reroot 3rd party libs
-  path = path.replace('../node_modules/moment/', '../lib/')
-  path = path.replace('../node_modules/superagent/', '../lib/')
-  path = path.replace('../node_modules/jquery/dist/', '../lib/')
-  path = path.replace('../node_modules/components-jqueryui/', '../lib/')
-  path = path.replace('../fullcalendar/dist/', '../lib/')
+let vendorPaths = []
 
-  // reroot dist files to archive root
-  path = path.replace('../dist/', '../')
+function transformDemoPath(path) {
+  path = path.replace('../dist/', '../packages/')
+  path = path.replace('../fullcalendar/dist/', '../packages/')
 
-  if (
-    !/\.min\.(js|css)$/.test(path) && // not already minified
-    !/^\w/.test(path) && // reference to demo util js/css file
-    path !== '../lib/superagent.js' // doesn't have a .min.js, but that's okay
-  ) {
-    // use minified
-    path = path.replace(/\/([^/]*)\.(js|css)$/, '/$1.min.$2')
-  }
+  path = path.replace(
+    /^\.\.\/node_modules\/(.*)\/([^/]+)$/,
+    function(m0, m1, m2) {
+      vendorPaths.push(m1 + '/' + m2)
+      return '../packages/' + m2
+    }
+  )
 
   return path
 }
