3f50247c42cd892ea4cfd26995df94b4a6147105
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-01 23:16:50 -0500

    make column resizing tests nicer

--- tests/automated/timeline/timeline-col-resizing.js
+++ tests/automated/timeline/timeline-col-resizing.js
@@ -1,38 +1,35 @@
 
-describe('timeline column resizing', function() {
+describe('timeline column resizing', function() { // better renamed to 'sizing'
+  pushOptions({
+    resourceAreaWidth: 230,
+    defaultView: 'resourceTimelineDay',
+    resourceColumns: [
+      {
+        labelText: 'Room',
+        field: 'title'
+      },
+      {
+        labelText: 'Occupancy',
+        field: 'occupancy'
+      }
+    ],
+    resourceGroupField: 'building',
+    resources: [
+      { id: 'a', occupancy: 40, building: '460 Bryant', title: 'Auditorium A' },
+      { id: 'b', occupancy: 40, building: '460 Bryant', title: 'Auditorium B', eventColor: 'green' }
+    ]
+  })
+
 
   it('works with resourceGroupField', function(done) {
-    initCalendar({
-      resourceAreaWidth: 230,
-      defaultView: 'resourceTimelineDay',
-      resourceColumns: [
-        {
-          labelText: 'Room',
-          field: 'title'
-        },
-        {
-          labelText: 'Occupancy',
-          field: 'occupancy'
-        }
-      ],
-      resourceGroupField: 'building',
-      resources: [
-        { id: 'a', occupancy: 40, building: '460 Bryant', title: 'Auditorium A' },
-        { id: 'b', occupancy: 40, building: '460 Bryant', title: 'Auditorium B', eventColor: 'green' }
-      ]
-    })
+    initCalendar()
 
     function expectColWidthsToMatch() {
-      const headCells = $('.fc-head .fc-resource-area th')
-      const bodyCells = $('.fc-body .fc-resource-area tr[data-resource-id="a"] td')
-      expect(headCells.length).toBe(2)
-      expect(bodyCells.length).toBe(2)
-
-      for (let i = 0; i < headCells.length; i++) {
-        const headCell = headCells[i]
-        const bodyCell = bodyCells[i]
-        expect($(headCell).width()).toBe($(bodyCell).width())
-      }
+      const headCellWidths = getHeadCellWidths()
+      const bodyCellWidths = getBodyCellWidths()
+      expect(headCellWidths.length).toBe(2)
+      expect(bodyCellWidths.length).toBe(2)
+      expect(headCellWidths).toEqual(bodyCellWidths)
     }
 
     expectColWidthsToMatch()
@@ -45,4 +42,18 @@ describe('timeline column resizing', function() {
       }
     })
   })
+
+
+  function getHeadCellWidths() { // rounded (inadvertently by jquery)
+    return $('.fc-head .fc-resource-area th').get().map(function(el) {
+      return $(el).width()
+    })
+  }
+
+  function getBodyCellWidths() { // rounded (inadvertently by jquery)
+    return $('.fc-body .fc-resource-area tr[data-resource-id="a"] td').get().map(function(el) {
+      return $(el).width()
+    })
+  }
+
 })
