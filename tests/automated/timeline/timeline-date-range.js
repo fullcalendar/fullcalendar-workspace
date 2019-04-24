3ecea9c8db7140dcfe2040c0922f985979b4c3d6
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-23 22:15:10 -0500

    test for issue 525

--- tests/automated/timeline/timeline-date-range.js
+++ tests/automated/timeline/timeline-date-range.js
@@ -1,3 +1,5 @@
+import { expectRenderRange } from 'fullcalendar/tests/automated/view-dates/ViewDateUtils'
+
 describe('timeline date range', function() {
 
   it('respects firstDay with auto-detected alignment with 7-days', function() {
@@ -14,6 +16,28 @@ describe('timeline date range', function() {
         $('.fc-head th[data-date]:first').data('date')
       )
     ).toEqualDate('2018-01-22') // a Monday
+  })
+
+  // https://github.com/fullcalendar/fullcalendar-scheduler/issues/525
+  xit('can go back by a month', function() {
+    initCalendar({
+      defaultDate: '2019-04-23',
+      defaultView: 'timelineSpecial',
+      views: {
+        timelineSpecial: {
+          type: 'timeline',
+          duration: { month: 3 },
+          slotDuration: { month: 1 },
+          dateIncrement: { months: 1 }
+        }
+      }
+    })
+
+    expectRenderRange('2019-04-01', '2019-07-01')
+    currentCalendar.prev()
+    expectRenderRange('2019-03-01', '2019-06-01')
+    currentCalendar.prev()
+    expectRenderRange('2019-02-01', '2019-05-01')
   })
 
 })
