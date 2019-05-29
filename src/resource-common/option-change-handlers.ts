5fdc6e3e592a21771e60ccfca2a25b7bd56d7199
Adam Shaw <arshaw@users.noreply.github.com>
2019-05-29 12:30:37 -0500

    change handlers used passed-in deepEquals, kill freezeRaw

--- src/resource-common/option-change-handlers.ts
+++ src/resource-common/option-change-handlers.ts
@@ -1,13 +1,13 @@
-import { Calendar, isValuesSimilar } from '@fullcalendar/core'
+import { Calendar } from '@fullcalendar/core'
 
 export default {
   resources: handleResources
 }
 
-function handleResources(newSourceInput, calendar: Calendar) {
+function handleResources(newSourceInput, calendar: Calendar, deepEquals) {
   let oldSourceInput = calendar.state.resourceSource._raw
 
-  if (!isValuesSimilar(oldSourceInput, newSourceInput, 2)) {
+  if (!deepEquals(oldSourceInput, newSourceInput)) {
     calendar.dispatch({
       type: 'RESET_RESOURCE_SOURCE',
       resourceSourceInput: newSourceInput
