a2524ca48210f619be9f2abcf6b2d4a820367b2b
Adam Shaw <arshaw@users.noreply.github.com>
2020-01-06 11:58:29 -0500

    the bundle package

---
+++ bundle/src/main.ts
@@ -0,0 +1,34 @@
+
+export * from '@fullcalendar/core'
+export * from '@fullcalendar/interaction'
+export * from '@fullcalendar/daygrid'
+export * from '@fullcalendar/timegrid'
+export * from '@fullcalendar/list'
+export * from '@fullcalendar/timeline'
+export * from '@fullcalendar/resource-common'
+export * from '@fullcalendar/resource-daygrid'
+export * from '@fullcalendar/resource-timegrid'
+export * from '@fullcalendar/resource-timeline'
+
+import { addDefaultPlugins } from '@fullcalendar/core'
+import interactionPlugin from '@fullcalendar/interaction'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+import timelinePlugin from '@fullcalendar/timeline'
+import resourceCommonPlugin from '@fullcalendar/resource-common'
+import resourceDayGridPlugin from '@fullcalendar/resource-daygrid'
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+
+addDefaultPlugins([
+  interactionPlugin,
+  dayGridPlugin,
+  timeGridPlugin,
+  listPlugin,
+  timelinePlugin,
+  resourceCommonPlugin,
+  resourceDayGridPlugin,
+  resourceTimeGridPlugin,
+  resourceTimelinePlugin
+])
