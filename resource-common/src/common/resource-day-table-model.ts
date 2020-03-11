5778875cca3cf53c70d33d0fa9ccd442eaeb3878
Adam Shaw <arshaw@users.noreply.github.com>
2020-03-11 00:29:12 -0400

    remove interactingSeg

--- resource-common/src/common/resource-day-table-model.ts
+++ resource-common/src/common/resource-day-table-model.ts
@@ -291,7 +291,6 @@ export abstract class VResourceJoiner<SegType extends Seg> {
     let affectedInstances = {}
     let transformedSegs = []
     let isEvent = false
-    let interactingSeg = null
 
     for (let i = 0; i < resourceCnt; i++) {
       let interaction = interactions[i]
@@ -306,7 +305,6 @@ export abstract class VResourceJoiner<SegType extends Seg> {
 
         __assign(affectedInstances, interaction.affectedInstances)
         isEvent = isEvent || interaction.isEvent
-        interactingSeg = interactingSeg || interaction.interactingSeg
       }
 
       if (interactions[resourceCnt]) { // one beyond. the all-resource
@@ -322,8 +320,7 @@ export abstract class VResourceJoiner<SegType extends Seg> {
     return {
       affectedInstances,
       segs: transformedSegs,
-      isEvent,
-      interactingSeg
+      isEvent
     }
   }
 
