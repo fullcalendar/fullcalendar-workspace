44ceef0ad9b66b6554804fdbc04334ce07791537
Adam Shaw <arshaw@users.noreply.github.com>
2022-09-07 01:25:19 -0400

    quick note

--- scripts/src/subrepo/meta/disappear.ts
+++ scripts/src/subrepo/meta/disappear.ts
@@ -1,6 +1,8 @@
 import { parseSubrepoArgs, getAllMetaFiles, rootDir } from '../../utils/subrepo'
 import { disappear } from '../../utils/git'
 
+// somehow call this ASAP, on installation
+
 export default async function(...rawArgs: string[]): Promise<void> {
   const { subrepos } = await parseSubrepoArgs(rawArgs)
 
