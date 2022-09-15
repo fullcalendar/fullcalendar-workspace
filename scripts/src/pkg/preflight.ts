f34e2735a3cd68fe72cbc76a56e28894fb0c3ff4
Adam Shaw <arshaw@users.noreply.github.com>
2022-09-15 18:17:45 -0400

    get preflight working

--- scripts/src/pkg/preflight.ts
+++ scripts/src/pkg/preflight.ts
@@ -1,8 +1,10 @@
+import { mkdir } from 'fs/promises'
 import { processSrcMeta, writeDistMeta } from '../utils/pkg-meta'
 
 export default async function(...args: string[]) {
   const dev = args.indexOf('--dev') !== -1
 
   const { distMeta } = await processSrcMeta(dev)
+  await mkdir('./dist', { recursive: true })
   await writeDistMeta(distMeta)
 }
