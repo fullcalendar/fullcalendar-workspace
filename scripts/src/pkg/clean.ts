f4863459247013eb8e4d84729b88c3c0164f780c
Adam Shaw <arshaw@users.noreply.github.com>
2022-09-28 15:35:59 +0100

    buildConfig in pkgs

--- scripts/src/pkg/clean.ts
+++ scripts/src/pkg/clean.ts
@@ -2,5 +2,4 @@ import { rm } from 'fs/promises'
 
 export default async function(...args: string[]) {
   await rm('./dist', { force: true, recursive: true })
-  // TODO: clean tsconfig.tsbuildinfo for non-pkg packages?
 }
