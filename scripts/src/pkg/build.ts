f01b809f725defde718cb9959302ef93096f9272
Adam Shaw <arshaw@users.noreply.github.com>
2022-10-25 17:10:12 +0200

    fix bug with cleaning

--- scripts/src/pkg/build.ts
+++ scripts/src/pkg/build.ts
@@ -68,7 +68,7 @@ export async function writeDistLicense(pkgAnalysis: PkgAnalysis): Promise<void> 
 }
 
 async function deleteBuiltFiles(pkgDir: string): Promise<void> {
-  const relPaths = await globby(pathsToDelete, { cwd: pkgDir })
+  const relPaths = await globby(pathsToDelete, { cwd: pkgDir, onlyFiles: false })
 
   await Promise.all(
     relPaths.map(async (relPath) => {
