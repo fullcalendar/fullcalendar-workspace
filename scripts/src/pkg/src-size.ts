b95a23d3208a15a2006985494d41282d91c6f737
Adam Shaw <arshaw@users.noreply.github.com>
2022-11-27 15:03:13 -0500

    improve src-size anaylsis

--- scripts/src/pkg/src-size.ts
+++ scripts/src/pkg/src-size.ts
@@ -1,99 +1,260 @@
-import { join as joinPaths } from 'path'
+import { join as joinPaths } from 'node:path'
+import { Blob } from 'node:buffer'
+import zlib from 'node:zlib'
+import { promisify } from 'node:util'
 import { readFile } from 'fs/promises'
 import * as esbuild from 'esbuild'
+import { ScriptContext } from '@fullcalendar/standard-scripts/utils/script-runner'
 import { execCapture } from '@fullcalendar/standard-scripts/utils/exec'
-import { Blob } from 'buffer'
 
-export default async function() {
-  const pkgDir = process.cwd()
-  const sizes = await getSrcSizes(pkgDir)
+const gzip = promisify(zlib.gzip)
 
-  console.table(pkgSrcSizesToStrs(sizes))
+export default async function(this: ScriptContext) {
+  const pkgDir = this.cwd
+  const pkgName = this.monorepoStruct.pkgDirToJson[pkgDir].name
+
+  const pkgSrcs = await getPkgSrcs(pkgDir)
+  const compiledRowMap = await compileSizes({ [pkgName]: pkgSrcs })
+
+  displayTable(compiledRowMap)
 }
 
-/*
-All values are bytes of minified code
-*/
-export interface PkgSrcSizes {
-  componentSize: number
-  nonComponentSize: number
-  totalSize: number
+// Console display
+// -------------------------------------------------------------------------------------------------
+
+export function displayTable(compiledRowMap: CompiledRowMap): void {
+  const formattedRowMap: { [rowName: string]: any } = {}
+  const rowNames = Object.keys(compiledRowMap)
+
+  for (const rowName of rowNames) {
+    const compiledRow = compiledRowMap[rowName]
+
+    formattedRowMap[rowName] = {
+      logic: formatBytes(compiledRow.logic),
+      component: formatBytes(compiledRow.component),
+      style: formatBytes(compiledRow.style),
+      total: formatBytes(compiledRow.total),
+    }
+  }
+
+  console.table(formattedRowMap)
 }
 
-export async function getSrcSizes(pkgDir: string): Promise<PkgSrcSizes> {
-  const componentSize = await getSrcSizeByType(pkgDir, 'tsx')
-  const nonComponentSize = await getSrcSizeByType(pkgDir, 'ts')
+// Compile sizes for one or more packages
+// -------------------------------------------------------------------------------------------------
 
+export type PkgSrcsMap = { [pkgName: string]: PkgSrcs }
+export type CompiledRow = PkgSizes & { total: number }
+export type CompiledRowMap = { [rowName: string]: CompiledRow }
+
+export async function compileSizes(
+  pkgSrcsMap: PkgSrcsMap,
+  pkgNames?: string[],
+): Promise<CompiledRowMap> {
+  if (!pkgNames) {
+    pkgNames = Object.keys(pkgSrcsMap)
+    pkgNames.sort()
+  }
+
+  const rowMap: CompiledRowMap = {}
+  const combinedSrcs: PkgSrcs = {
+    logic: [],
+    component: [],
+    style: [],
+  }
+  const combinedSizes: PkgSizes = {
+    logic: 0,
+    component: 0,
+    style: 0,
+  }
+
+  for (const pkgName of pkgNames) {
+    const pkgSrcs = pkgSrcsMap[pkgName]
+    const pkgSizes = computePkgSizes(pkgSrcs)
+
+    rowMap[pkgName] = compileRow(pkgSizes)
+
+    combinedSrcs.logic.push(...pkgSrcs.logic)
+    combinedSrcs.component.push(...pkgSrcs.component)
+    combinedSrcs.style.push(...pkgSrcs.style)
+
+    combinedSizes.logic += pkgSizes.logic
+    combinedSizes.component += pkgSizes.component
+    combinedSizes.style += pkgSizes.style
+  }
+
+  if (pkgNames.length > 1) {
+    rowMap.TOTAL = compileRow(combinedSizes)
+  }
+
+  const minGzSizes = await computeMinGzPkgSizes(combinedSrcs)
+  rowMap['MIN+GZ'] = compileRow(minGzSizes)
+
+  return rowMap
+}
+
+function compileRow(pkgSizes: PkgSizes): CompiledRow {
   return {
-    componentSize,
-    nonComponentSize,
-    totalSize: componentSize + nonComponentSize,
+    ...pkgSizes,
+    total: pkgSizes.logic + pkgSizes.component + pkgSizes.style,
   }
 }
 
-async function getSrcSizeByType(pkgDir: string, loader: esbuild.Loader): Promise<number> {
+// Minified+gzip SIZES
+// -------------------------------------------------------------------------------------------------
+
+async function computeMinGzPkgSizes(pkgSrcs: PkgSrcs): Promise<PkgSizes> {
+  const [logic, component, style] = await Promise.all([
+    computeJsMinGzSize(pkgSrcs.logic),
+    computeJsMinGzSize(pkgSrcs.component),
+    computeCssMinGzSize(pkgSrcs.style),
+  ])
+
+  return { logic, component, style }
+}
+
+// Minified+gzip SOURCES
+// -------------------------------------------------------------------------------------------------
+
+async function computeJsMinGzSize(srcs: string[]): Promise<number> {
+  const minifiedSrc = (await Promise.all(
+    srcs.map(async (src) => {
+      return (await esbuild.transform(src, {
+        loader: 'jsx',
+        jsxFactory: 'h',
+        minify: true,
+      })).code
+    }),
+  )).join('\n')
+
+  // return computeStrSize([minifiedSrc])
+  return await computeGzipSize(minifiedSrc)
+}
+
+async function computeCssMinGzSize(srcs: string[]): Promise<number> {
+  const massagedSrc = srcs.join('\n').replace(/\s+/g, ' ')
+
+  // return computeStrSize([massagedSrc])
+  return await computeGzipSize(massagedSrc)
+}
+
+async function computeGzipSize(str: string): Promise<number> {
+  return (await gzip(str, { level: 9 })).length
+}
+
+// Source code SIZES per-package
+// -------------------------------------------------------------------------------------------------
+
+interface PkgSizes {
+  logic: number
+  component: number
+  style: number
+}
+
+function computePkgSizes(pkgSrcs: PkgSrcs): PkgSizes {
+  return {
+    logic: computeStrSize(pkgSrcs.logic),
+    component: computeStrSize(pkgSrcs.component),
+    style: computeStrSize(pkgSrcs.style),
+  }
+}
+
+function computeStrSize(str: string[]): number {
+  return new Blob(str).size
+}
+
+// Source code STRINGS per-package
+// -------------------------------------------------------------------------------------------------
+
+export interface PkgSrcs {
+  logic: string[]
+  component: string[]
+  style: string[]
+}
+
+export async function getPkgSrcs(pkgDir: string): Promise<PkgSrcs> {
+  const [logic, component, style] = await Promise.all([
+    getLogicSrcs(pkgDir),
+    getComponentSrcs(pkgDir),
+    getStyleSrcs(pkgDir),
+  ])
+  return { logic, component, style }
+}
+
+// Type-specific source code
+// -------------------------------------------------------------------------------------------------
+
+async function getLogicSrcs(pkgDir: string): Promise<string[]> {
+  const srcsMap = await getRawSrcs(pkgDir, '.ts')
+
+  return Promise.all(
+    Object.keys(srcsMap).map(async (path) => {
+      const src = srcsMap[path]
+      const transpiledSrc = (await esbuild.transform(src, {
+        loader: 'ts',
+      })).code
+
+      return transpiledSrc
+    }),
+  )
+}
+
+async function getComponentSrcs(pkgDir: string): Promise<string[]> {
+  const srcsMap = await getRawSrcs(pkgDir, '.tsx')
+
+  return Promise.all(
+    Object.keys(srcsMap).map(async (path) => {
+      const src = srcsMap[path]
+      const transpiledSrc = (await esbuild.transform(src, {
+        loader: 'tsx',
+        jsx: 'preserve',
+      })).code
+
+      if (
+        !transpiledSrc.match(/\bh\(/) &&
+        !transpiledSrc.match(/\bextends \w*Component\b/) &&
+        !src.match(/\bpreact\b/) // importing types from typescript src
+      ) {
+        console.warn(`[NO JSX/PREACT]: ${path}`)
+      }
+
+      return transpiledSrc
+    }),
+  )
+}
+
+async function getStyleSrcs(pkgDir: string): Promise<string[]> {
+  return Object.values(await getRawSrcs(pkgDir, '.css'))
+}
+
+// Source code retrieval
+// -------------------------------------------------------------------------------------------------
+
+type SrcsMap = { [path: string]: string }
+
+async function getRawSrcs(pkgDir: string, extension: string): Promise<SrcsMap> {
   const filesRaw = await execCapture([
-    'find', 'src', '-type', 'f', '-name', '*.' + loader,
+    'find', 'src', '-type', 'f', '-name', '*' + extension,
   ], {
     cwd: pkgDir,
   })
   const fileSubpaths = splitLines(filesRaw)
-  const allLines: string[] = []
+  const srcsMap: SrcsMap = {}
 
-  for (const fileSubpath of fileSubpaths) {
-    const filePath = joinPaths(pkgDir, fileSubpath)
-    const srcCode = await readFile(filePath, 'utf8')
+  await Promise.all(
+    fileSubpaths.map(async (fileSubpath) => {
+      const filePath = joinPaths(pkgDir, fileSubpath)
+      srcsMap[filePath] = await readFile(filePath, 'utf8')
+    }),
+  )
 
-    // removes typescript and comments
-    // because most example projects that measure size are in JSX with minimal comments
-    const transpiledRes = await esbuild.transform(srcCode, {
-      loader,
-      jsxFactory: 'h',
-    })
-    const transpiledCode = transpiledRes.code
-
-    // console.log()
-    // console.log()
-    // console.log(transpiledCode)
-    // console.log()
-    // console.log()
-
-    if (
-      (loader === 'tsx' || loader === 'jsx') &&
-      !transpiledCode.match(/\bh\(/) &&
-      !transpiledCode.match(/\bextends \w*Component\b/) &&
-      !srcCode.match(/\bpreact\b/) // importing types from typescript src
-    ) {
-      console.error(`[NO JSX/PREACT]: ${filePath}`)
-    }
-
-    allLines.push(transpiledCode)
-  }
-
-  return new Blob(allLines).size
-}
-
-// PkgSrcSize utils
-// -------------------------------------------------------------------------------------------------
-
-export interface PkgSrcSizeStrs {
-  componentSize: string
-  nonComponentSize: string
-  totalSize: string
-}
-
-export function pkgSrcSizesToStrs(sizes: PkgSrcSizes): PkgSrcSizeStrs {
-  return {
-    componentSize: formatBytes(sizes.componentSize),
-    nonComponentSize: formatBytes(sizes.nonComponentSize),
-    totalSize: formatBytes(sizes.totalSize),
-  }
+  return srcsMap
 }
 
 // General utils
 // -------------------------------------------------------------------------------------------------
 
-export function formatBytes(bytes: number, decimals = 2) {
+function formatBytes(bytes: number, decimals = 2) {
   if (!+bytes) return '0 Bytes'
 
   const k = 1024
--- scripts/src/src-size.ts
+++ scripts/src/src-size.ts
@@ -1,31 +1,35 @@
 import { ScriptContext } from '@fullcalendar/standard-scripts/utils/script-runner'
 import { PkgStruct, traverseMonorepoGreedy } from '@fullcalendar/standard-scripts/utils/monorepo-struct'
 import { analyzePkg } from '@fullcalendar/standard-scripts/utils/pkg-analysis'
-import { PkgSrcSizes, PkgSrcSizeStrs, getSrcSizes, pkgSrcSizesToStrs } from './pkg/src-size.js'
+import { PkgSrcs, getPkgSrcs, compileSizes, displayTable } from './pkg/src-size.js'
 
 export default async function(this: ScriptContext) {
   const { monorepoStruct } = this
-  const totalSizes: PkgSrcSizes = { componentSize: 0, nonComponentSize: 0, totalSize: 0 }
-  const rows: { [name: string]: PkgSrcSizeStrs } = {}
+  const pkgSrcsMap: { [name: string]: PkgSrcs } = {}
+  const bundles: { [name: string]: string[] } = {}
 
   await traverseMonorepoGreedy(monorepoStruct, async (pkgStruct: PkgStruct) => {
     const { pkgDir, pkgJson } = pkgStruct
     const pkgAnalysis = analyzePkg(pkgDir)
 
-    if (
-      pkgJson.buildConfig &&
-      !pkgAnalysis.isBundle &&
-      !pkgAnalysis.isTests
-    ) {
-      const sizes = await getSrcSizes(pkgDir)
-      rows[pkgJson.name] = pkgSrcSizesToStrs(sizes)
-
-      totalSizes.componentSize += sizes.componentSize
-      totalSizes.nonComponentSize += sizes.nonComponentSize
-      totalSizes.totalSize += sizes.totalSize
+    if (pkgJson.buildConfig) {
+      if (pkgAnalysis.isBundle) {
+        bundles[pkgJson.name] = Object.keys(pkgJson.dependencies)
+      } else if (!pkgAnalysis.isTests) {
+        pkgSrcsMap[pkgJson.name] = await getPkgSrcs(pkgDir)
+      }
     }
   })
 
-  rows.TOTAL = pkgSrcSizesToStrs(totalSizes)
-  console.table(rows)
+  const compiledRowMap = await compileSizes(pkgSrcsMap)
+  displayTable(compiledRowMap)
+
+  for (const bundleName in bundles) {
+    const pkgNames = bundles[bundleName]
+    const compiledRowMap = await compileSizes(pkgSrcsMap, pkgNames)
+
+    console.log()
+    console.log(bundleName)
+    displayTable(compiledRowMap)
+  }
 }
