2cd321e9e94db10322bb3eae34cf936e72bdfcde
Adam Shaw <arshaw@users.noreply.github.com>
2022-12-13 20:26:04 -0500

    proper non-beta versions

--- scripts/bin/publish.sh
+++ scripts/bin/publish.sh
@@ -5,38 +5,38 @@ set -e
 
 # first-time publishes need `--access public`
 
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/core && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/bootstrap5 && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/daygrid && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/timegrid && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/bootstrap && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/google-calendar && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/icalendar && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/interaction && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/list && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/luxon && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/luxon2 && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/moment && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/moment-timezone && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/standard/packages/rrule && pnpm publish --no-git-checks --tag beta
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/core && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/bootstrap5 && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/daygrid && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/timegrid && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/bootstrap && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/google-calendar && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/icalendar && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/interaction && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/list && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/luxon && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/luxon2 && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/moment && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/moment-timezone && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/standard/packages/rrule && pnpm publish --no-git-checks
 cd /Users/adam/Code/fullcalendar-workspace/standard/packages/web-component && pnpm publish --no-git-checks
-cd /Users/adam/Code/fullcalendar-workspace/standard/bundle && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/premium-common && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/adaptive && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/scrollgrid && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource-daygrid && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/timeline && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource-timegrid && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource-timeline && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/premium/bundle && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/contrib/react && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/contrib/angular && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/contrib/vue3 && pnpm publish --no-git-checks --tag beta
-cd /Users/adam/Code/fullcalendar-workspace/contrib/vue2 && pnpm publish --no-git-checks --tag beta
+cd /Users/adam/Code/fullcalendar-workspace/standard/bundle && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/premium-common && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/adaptive && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/scrollgrid && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource-daygrid && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/timeline && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource-timegrid && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/packages/resource-timeline && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/premium/bundle && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/contrib/react && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/contrib/angular && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/contrib/vue3 && pnpm publish --no-git-checks
+cd /Users/adam/Code/fullcalendar-workspace/contrib/vue2 && pnpm publish --no-git-checks
 cd /Users/adam/Code/fullcalendar-workspace
 
 # tag pushing (DO SPECIAL for vue versions)
-# git tag -a v6.0.0-beta.4 -m v6.0.0-beta.4 && git push origin v6.0.0-beta.4
-# git tag -a v6.0.0-vue2beta.4 -m v6.0.0-vue2beta.4 && git push origin v6.0.0-vue2beta.4
-# git tag -a v6.0.0-vue3beta.4 -m v6.0.0-vue3beta.4 && git push origin v6.0.0-vue3beta.4
+# git tag -a v6.0.0 -m v6.0.0 && git push origin v6.0.0
+# git tag -a v6.0.0-vue2 -m v6.0.0-vue2 && git push origin v6.0.0-vue2
+# git tag -a v6.0.0-vue3 -m v6.0.0-vue3 && git push origin v6.0.0-vue3
