1345f08e39db0ef8eb1adbae3ca8ff3e8b952897
Adam Shaw <arshaw@users.noreply.github.com>
2023-09-20 18:39:27 -0400

    test for late registerActiveComponent for zero-cell TableRows

---
+++ premium/tests/src/column/column-resource-fetch.ts
@@ -0,0 +1,49 @@
+import { CalendarWrapper } from '@fullcalendar/standard-tests/lib/wrappers/CalendarWrapper'
+
+describe('vresource resource refetching', () => {
+  const initialDate = '2023-09-20'
+  const events = [
+    { start: initialDate, title: 'event', allDay: true, resourceId: 'a' }
+  ]
+  const resources = [
+    { id: 'a', title: 'Auditorium A' },
+    { id: 'b', title: 'Auditorium B' },
+    { id: 'c', title: 'Auditorium C' },
+  ]
+
+  pushOptions({
+    initialDate,
+    initialView: 'resourceTimeGridDay',
+    events,
+    editable: true,
+  })
+
+  // https://github.com/fullcalendar/fullcalendar/issues/7365
+  it('leaves all-day resources interactable', (done) => {
+    let eventClickCnt = 0
+
+    let calendar = initCalendar({
+      // resources,
+      resources(fetchInfo, successCallback) {
+        setTimeout(() => {
+          successCallback(resources)
+        }, 100)
+      },
+      eventClick() {
+        eventClickCnt++
+      },
+      dateClick() {
+        console.log('yypppppp')
+      }
+    })
+
+    setTimeout(() => {
+      let eventEl = new CalendarWrapper(calendar).getEventEls()[0]
+      $(eventEl).simulate('click')
+      setTimeout(() => {
+        expect(eventClickCnt).toBe(1)
+        done()
+      }, 0)
+    }, 200)
+  })
+})
