59f07a795b4d6563e99c0d89459b8e5ffb14c28b
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-07 11:19:05 -0500

    strip some stuff out

--- scripts/src/meta/utils.ts
+++ scripts/src/meta/utils.ts
@@ -1,25 +1,17 @@
-import { join as joinPaths } from 'path'
-import { execCapture } from '@fullcalendar/standard-scripts/utils/exec'
-import { fileExists } from '@fullcalendar/standard-scripts/utils/fs'
 
 // Git utils
 // -------------------------------------------------------------------------------------------------
 
+// TEMPORARY. RENAME.
 export async function queryGitSubmodulePkgs(monorepoDir: string): Promise<string[]> {
-  let submoduleSubdirs = await queryGitSubmoduleDirs(monorepoDir)
-
-  return await asyncFilter(submoduleSubdirs, (subdir) => {
-    return fileExists(joinPaths(subdir, 'package.json'))
-  })
-}
-
-async function queryGitSubmoduleDirs(monorepoDir: string): Promise<string[]> {
-  const s = await execCapture(['git', 'submodule', 'status'], { cwd: monorepoDir })
-  const lines = s.trim().split('\n')
-
-  return lines.map((line) => {
-    return line.trim().split(' ')[1]
-  })
+  return [
+    'standard',
+    'examples',
+    'contrib/angular',
+    'contrib/react',
+    'contrib/vue2',
+    'contrib/vue3',
+  ]
 }
 
 // Lang utils
