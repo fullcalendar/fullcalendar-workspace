e3795a4cbaa702d753ac1c82d5a5c906e0a8ae2d
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-08 13:51:32 -0500

    better lockfile creation script

--- premium/packages/pnpm-make-dedicated-lockfile/lib.js
+++ premium/packages/pnpm-make-dedicated-lockfile/lib.js
@@ -1,99 +1,135 @@
 // derived from https://github.com/pnpm/pnpm/blob/main/packages/make-dedicated-lockfile/src/index.ts
 
 import { spawn } from 'child_process'
-import { join as joinPaths } from 'path'
-import { writeFile, unlink as unlinkFile, lstat } from 'fs/promises'
-import { createExportableManifest } from '@pnpm/exportable-manifest'
+import { join as joinPaths, sep as pathSep, isAbsolute } from 'path'
 import { getLockfileImporterId, readWantedLockfile, writeWantedLockfile } from '@pnpm/lockfile-file'
 import { pruneSharedLockfile } from '@pnpm/prune-lockfile'
-import pickBy from '@pnpm/ramda/src/pickBy' // a fork of ramda by PNPM
-import { readProjectManifest } from '@pnpm/read-project-manifest'
 import { DEPENDENCIES_FIELDS } from '@pnpm/types'
 
-const subWorkspaceTempContents = `
-# temporary config for scoped PNPM lockfile generation
-packages: []
-`
-
-export async function makeDedicatedLockfile(lockfileDir, projectDir, verbose) {
-  const lockfile = await readWantedLockfile(lockfileDir, { ignoreIncompatible: false })
+export async function makeDedicatedLockfile(rootDir, scopedDir, verbose) {
+  const lockfile = await readWantedLockfile(rootDir, { ignoreIncompatible: false })
   if (lockfile == null) {
     throw new Error('no lockfile found')
   }
 
-  // Create a tentative scoped pnpm-lock.yaml
-  const allImporters = lockfile.importers
-  lockfile.importers = {}
-  const baseImporterId = getLockfileImporterId(lockfileDir, projectDir)
-  for (const [importerId, importer] of Object.entries(allImporters)) {
-    if (importerId.startsWith(`${baseImporterId}/`)) {
-      const newImporterId = importerId.slice(baseImporterId.length + 1)
-      lockfile.importers[newImporterId] = projectSnapshotWithoutLinkedDeps(importer)
-      continue
-    }
-    if (importerId === baseImporterId) {
-      lockfile.importers['.'] = projectSnapshotWithoutLinkedDeps(importer)
-    }
+  if (!isAbsolute(scopedDir)) {
+    scopedDir = joinPaths(rootDir, scopedDir)
   }
+
+  const baseImporterId = getLockfileImporterId(rootDir, scopedDir)
+  const pkgsOutOfScope = []
+
+  if (lockfile.patchedDependencies) {
+    const transformedPatchedDeps = {}
+
+    for (const [dependencyId, patchData] of Object.entries(lockfile.patchedDependencies)) {
+      const patchPath = patchData.path
+      const scopedPatchPath =
+        patchPath.startsWith(`${baseImporterId}/`) &&
+        patchPath.slice(baseImporterId.length + 1)
+
+      if (scopedPatchPath) {
+        transformedPatchedDeps[dependencyId] = { ...patchData, path: scopedPatchPath }
+      }
+    }
+
+    lockfile.patchedDependencies = transformedPatchedDeps
+  }
+
+  if (lockfile.importers) {
+    const transformedImporters = {}
+
+    for (const [importerId, snapshot] of Object.entries(lockfile.importers)) {
+      let scopedImporterId =
+        importerId.startsWith(`${baseImporterId}/`)
+          ? importerId.slice(baseImporterId.length + 1)
+          : (importerId === baseImporterId ? '.' : undefined)
+
+      if (scopedImporterId) {
+        transformedImporters[scopedImporterId] = filterLinkedDepsOutOfScope(
+          scopedImporterId,
+          snapshot,
+          scopedDir,
+          pkgsOutOfScope, // appended to
+        )
+      }
+    }
+
+    lockfile.importers = transformedImporters
+  }
+
   const dedicatedLockfile = pruneSharedLockfile(lockfile)
-  await writeWantedLockfile(projectDir, dedicatedLockfile)
+  await writeWantedLockfile(scopedDir, dedicatedLockfile)
+  console.log('Lockfile written.')
 
-  // Possibly update package.json, for some reason
-  const { manifest, writeProjectManifest } = await readProjectManifest(projectDir)
-  const publishManifest = await createExportableManifest(projectDir, manifest)
-  await writeProjectManifest(publishManifest)
-
-  // Possible create a temporary pnpm-workspace.yaml, to simulate a monorepo root
-  const subWorkspaceConfigPath = joinPaths(projectDir, 'pnpm-workspace.yaml')
-  const subWorkspaceConfigIsTemp = !(await fileExists(subWorkspaceConfigPath))
-  if (subWorkspaceConfigIsTemp) {
-    await writeFile(subWorkspaceConfigPath, subWorkspaceTempContents)
-  }
-
-  // Run a real install to clean up pnpm-lock.yaml
-  let installError
-  try {
-    await exec(['pnpm', 'install', '--ignore-scripts'], {
-      cwd: projectDir,
+  if (pkgsOutOfScope.length) {
+    if (verbose) {
+      console.log(
+        'However, cannot fixup generated lockfile with a reinstall due to the following linked packages being\n' +
+        'out of the scope of ' + scopedDir + ':\n' +
+        '- ' + pkgsOutOfScope.join('\n- ') + '\n'
+      )
+    }
+  } else {
+    if (verbose) {
+      console.log('Attempting fixup of generated lockfile with a reinstall:\n')
+    }
+    await exec([
+      'pnpm',
+      'install',
+      '--ignore-scripts',
+      '--resolution-only',
+      '--lockfile-dir=.',
+      '--filter=.',
+    ], {
+      cwd: scopedDir,
       stdio: verbose ? 'inherit' : 'ignore',
     })
-  } catch (error) {
-    installError = error
-  }
-
-  // Undo temporary pnpm-workspace.yaml
-  if (subWorkspaceConfigIsTemp) {
-    await unlinkFile(subWorkspaceConfigPath)
-  }
-
-  if (installError) {
-    throw installError
   }
 }
 
-function projectSnapshotWithoutLinkedDeps(projectSnapshot) {
-  const newProjectSnapshot = {
-    specifiers: projectSnapshot.specifiers,
-  }
-  for (const depField of DEPENDENCIES_FIELDS) {
-    if (projectSnapshot[depField] == null) continue
-    newProjectSnapshot[depField] = pickBy(
-      (depVersion) => !depVersion.startsWith('link:'),
-      projectSnapshot[depField],
-    )
-  }
-  return newProjectSnapshot
-}
-
-// Utils (TODO: make DRY)
+// Linked Deps
 // -------------------------------------------------------------------------------------------------
 
-function fileExists(path) {
-  return lstat(path).then(
-    () => true,
-    () => false,
-  )
+const linkPrefix = 'link:'
+
+function filterLinkedDepsOutOfScope(importerId, snapshot, scopedDir, pkgsOutOfScope) {
+  const transformedSnapshot = {}
+
+  for (const [snapshotKey, snapshotVal] of Object.entries(snapshot)) {
+    if (!DEPENDENCIES_FIELDS.includes(snapshotKey)) {
+      // not a dependency-related field. copy as-is
+      transformedSnapshot[snapshotKey] = snapshotVal
+    } else if (snapshotVal) {
+      const filteredDeps = {}
+
+      for (const [depId, depLocator] of Object.entries(snapshotVal)) {
+        const depLink =
+          depLocator.startsWith(linkPrefix) &&
+          depLocator.slice(linkPrefix.length)
+
+        if (
+          depLink && (
+            isAbsolute(depLink) ||
+            // relative, but references path outside of scopedDir?
+            !joinPaths(scopedDir, importerId, depLink).startsWith(scopedDir + pathSep)
+          )
+        ) {
+          pkgsOutOfScope.push(depId)
+        } else {
+          filteredDeps[depId] = depLocator
+        }
+      }
+
+      transformedSnapshot[snapshotKey] = filteredDeps
+    }
+  }
+
+  return transformedSnapshot
 }
+
+// General Utils (TODO: make DRY)
+// -------------------------------------------------------------------------------------------------
 
 function exec(cmdParts, options = {}) {
   return new Promise((resolve, reject) => {
