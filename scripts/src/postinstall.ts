6e68f8f3bf28a162e2649dd50f4bcdf84cbf55ef
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-09 23:46:04 -0500

    dont hide files on CI

--- scripts/src/postinstall.ts
+++ scripts/src/postinstall.ts
@@ -5,6 +5,11 @@ import { hideMetaFiles } from './meta/hide.js'
 export default async function(this: ScriptContext) {
   await Promise.all([
     origPostinstall.call(this), // TODO: weird. use non-cli utility
-    hideMetaFiles(process.cwd(), true), // silent=true
+
+    // If in a GitHub Action running against a PR, it's not a real branch (just a commit)
+    // and thus github-subrepo fails. We don't care about hiding these files in CI anyway.
+    // They're just for ergonomic reasons while developing.
+    !process.env.CI &&
+      hideMetaFiles(process.cwd(), true), // silent=true
   ])
 }
