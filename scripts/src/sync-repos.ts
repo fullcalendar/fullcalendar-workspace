c06d4520e7df3c69a910914b6f4f7039748e40d8
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-12 16:42:34 -0500

    try fetch first

--- scripts/src/sync-repos.ts
+++ scripts/src/sync-repos.ts
@@ -5,6 +5,9 @@ export default async function() {
   const monorepoDir = process.cwd()
   const gitSubrepoBin = joinPaths(monorepoDir, 'scripts/bin/git-subrepo.sh') // TODO: DRY
 
+  // Try...
+  await execLive([gitSubrepoBin, 'fetch', '--all'])
+
   // Push to subrepos. Will fail if our copies are not up-to-date
   await execLive([gitSubrepoBin, 'push', '--all'])
 
