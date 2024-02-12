ea65725bebf04f387ca553783189d51678e82350
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-12 17:09:07 -0500

    mooooore debug

--- scripts/src/sync-repos.ts
+++ scripts/src/sync-repos.ts
@@ -9,12 +9,21 @@ export default async function() {
   await execLive([gitSubrepoBin, 'fetch', '--all'])
 
   console.log('TEST!!!0...')
+  await execLive('git -v')
+
+  console.log('TEST!!!1...')
   await execLive('git status')
 
   console.log('TEST!!!2...')
-  await execLive('git diff 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
+  await execLive('git show --stat 290b11a7f965b5211ce9caf9df451fdf6fe1791e')
 
   console.log('TEST!!!3...')
+  await execLive('git show --stat HEAD')
+
+  console.log('TEST!!!4...')
+  await execLive('git diff 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
+
+  console.log('TEST!!!5...')
   await execLive('git rev-list --reverse --ancestry-path --topo-order 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
 
   // Push to subrepos. Will fail if our copies are not up-to-date
