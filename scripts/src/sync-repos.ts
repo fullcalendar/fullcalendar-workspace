1041b1de94e9a2af7ce4fe221091a40bf28eb41d
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-12 17:14:05 -0500

    disable some debugging

--- scripts/src/sync-repos.ts
+++ scripts/src/sync-repos.ts
@@ -5,8 +5,8 @@ export default async function() {
   const monorepoDir = process.cwd()
   const gitSubrepoBin = joinPaths(monorepoDir, 'scripts/bin/git-subrepo.sh') // TODO: DRY
 
-  // Try...
-  await execLive([gitSubrepoBin, 'fetch', '--all'])
+  // // Try...
+  // await execLive([gitSubrepoBin, 'fetch', '--all'])
 
   console.log('TEST!!!0...')
   await execLive('git -v')
@@ -14,17 +14,17 @@ export default async function() {
   console.log('TEST!!!1...')
   await execLive('git status')
 
-  console.log('TEST!!!2...')
-  await execLive('git show --stat 290b11a7f965b5211ce9caf9df451fdf6fe1791e')
+  // console.log('TEST!!!2...')
+  // await execLive('git show --stat 290b11a7f965b5211ce9caf9df451fdf6fe1791e')
 
-  console.log('TEST!!!3...')
-  await execLive('git show --stat HEAD')
+  // console.log('TEST!!!3...')
+  // await execLive('git show --stat HEAD')
 
-  console.log('TEST!!!4...')
-  await execLive('git diff 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
+  // console.log('TEST!!!4...')
+  // await execLive('git diff 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
 
-  console.log('TEST!!!5...')
-  await execLive('git rev-list --reverse --ancestry-path --topo-order 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
+  // console.log('TEST!!!5...')
+  // await execLive('git rev-list --reverse --ancestry-path --topo-order 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
 
   // Push to subrepos. Will fail if our copies are not up-to-date
   await execLive([gitSubrepoBin, 'push', '--all', '--DEBUG'])
