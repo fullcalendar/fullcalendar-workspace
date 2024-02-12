3b771a284ea29d9225073ce7c5dcc1baede9959e
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-12 16:54:34 -0500

    more deguggg

--- scripts/src/sync-repos.ts
+++ scripts/src/sync-repos.ts
@@ -5,8 +5,11 @@ export default async function() {
   const monorepoDir = process.cwd()
   const gitSubrepoBin = joinPaths(monorepoDir, 'scripts/bin/git-subrepo.sh') // TODO: DRY
 
-  // // Try...
-  // await execLive([gitSubrepoBin, 'fetch', '--all'])
+  // Try...
+  await execLive([gitSubrepoBin, 'fetch', '--all'])
+
+  console.log('TEST!!!...')
+  await execLive('git rev-list --reverse --ancestry-path --topo-order 290b11a7f965b5211ce9caf9df451fdf6fe1791e..HEAD')
 
   // Push to subrepos. Will fail if our copies are not up-to-date
   await execLive([gitSubrepoBin, 'push', '--all', '--debug'])
