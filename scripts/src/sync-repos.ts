0ad67a202d5bfaf887d6735a6ac29951df38e49b
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-12 16:47:09 -0500

    verbose

--- scripts/src/sync-repos.ts
+++ scripts/src/sync-repos.ts
@@ -5,11 +5,11 @@ export default async function() {
   const monorepoDir = process.cwd()
   const gitSubrepoBin = joinPaths(monorepoDir, 'scripts/bin/git-subrepo.sh') // TODO: DRY
 
-  // Try...
-  await execLive([gitSubrepoBin, 'fetch', '--all'])
+  // // Try...
+  // await execLive([gitSubrepoBin, 'fetch', '--all'])
 
   // Push to subrepos. Will fail if our copies are not up-to-date
-  await execLive([gitSubrepoBin, 'push', '--all'])
+  await execLive([gitSubrepoBin, 'push', '--all', '--verbose'])
 
   // Push the git-subrepo meta-file updates back to our remote
   await execLive(['git', 'push'])
