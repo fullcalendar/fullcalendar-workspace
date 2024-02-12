014c307d986108adaf63700c74c150c23dd2495b
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-12 14:36:28 -0500

    simplify with gh

--- scripts/src/tag-and-release.ts
+++ scripts/src/tag-and-release.ts
@@ -1,8 +1,6 @@
-import { basename, join as joinPaths } from 'path'
-import { readFile } from 'fs/promises'
-import { Octokit } from 'octokit'
+import { join as joinPaths } from 'path'
 import { execLive } from '@fullcalendar-scripts/standard/utils/exec'
-import { getSubrepo, getSubrepos, readManifest } from './meta/utils.js'
+import { getSubrepos, readManifest } from './meta/utils.js'
 import { changelogSrc, getChangelogEntry } from './version-notes.js'
 
 export default async function() {
@@ -41,43 +39,22 @@ export default async function() {
 
 async function tagAndReleaseRoot(monorepoDir: string, version: string): Promise<void> {
   const execOpts = { cwd: monorepoDir }
-  const isCi = Boolean(process.env.CI)
-  const githubToken = process.env.GITHUB_TOKEN
-  const githubRepo = process.env.GITHUB_REPOSITORY || 'fullcalendar/fullcalendar-workspace' // TODO: read this from manifest
   const tagName = `v${version}`
 
-  // Create Git tag
   console.log(`Creating root tag ${tagName}...`)
   await execLive(['git', 'tag', '-a', tagName, '-m', tagName], execOpts)
 
-  // Push Git tag
-  if (githubToken) {
-    console.log('Pushing root tag to remote with credentials...')
-    const secretUrl = `https://${githubToken}@github.com/${githubRepo}.git`
-    await execLive(['git', 'push', secretUrl, tagName], execOpts)
-  } else if (!isCi) {
-    console.log('Pushing root tag to remote...')
-    await execLive(['git', 'push', 'origin', tagName], execOpts)
-  } else {
-    throw new Error('Must specify GITHUB_TOKEN')
-  }
+  console.log('Pushing root tag to remote...')
+  await execLive(['git', 'push', 'origin', tagName], execOpts)
 
-  // Create GitHub release
-  if (githubToken) {
-    await createGithubRelease(
-      githubToken,
-      githubRepo,
-      tagName,
-      version,
-      monorepoDir,
-      `premium/dist/fullcalendar-scheduler-${version}.zip`,
-      true, // linkToStandard
-    )
-  } else if (!isCi) {
-    console.log('Skipping release creation')
-  } else {
-    throw new Error('Must specify GITHUB_TOKEN')
-  }
+  await createGithubRelease(
+    'fullcalendar/fullcalendar-workspace', // TODO: read this from manifest
+    tagName,
+    version,
+    monorepoDir,
+    `premium/dist/fullcalendar-scheduler-${version}.zip`,
+    true, // linkToStandard
+  )
 }
 
 async function tagAndReleaseSubrepo(
@@ -91,84 +68,33 @@ async function tagAndReleaseSubrepo(
   }
 
   const execOpts = { cwd: monorepoDir }
-  const isCi = Boolean(process.env.CI)
+  const tagName = `v${version}`
+  const tempTagName = `subrepo/${subrepoSubdir}/${tagName}`
   const subrepoRemote = subrepo['remote-url']
-  let subrepoCommit = subrepo['pulled-commit']
+  const subrepoCommit = subrepo['pulled-commit']
 
-  if (!subrepoRemote || !subrepoCommit) {
-    throw new Error('Could not determine subrepo remote/commit')
-  }
-
-  const githubTokenName = `GITHUB_${basename(subrepoSubdir).toUpperCase()}_TOKEN`
-  const githubToken = process.env[githubTokenName]
-  const githubRepo = remoteToGithubRepo(subrepoRemote)
-  const secretUrl = githubToken && `https://${githubToken}@github.com/${githubRepo}.git`
-  const gitSubrepoBin = joinPaths(monorepoDir, 'scripts/bin/git-subrepo.sh') // TODO: DRY
-
-  // Push subrepo
-  if (secretUrl) {
-    console.log('Pushing subrepo branch to remote with credentials...')
-    await execLive([gitSubrepoBin, 'push', subrepoSubdir, '-r', secretUrl], execOpts)
-  } else if (!isCi) {
-    console.log('Pushing subrepo branch to remote...')
-    await execLive([gitSubrepoBin, 'push', subrepoSubdir], execOpts)
-  } else {
-    throw new Error(`Must specify ${githubTokenName} environment variable`)
-  }
-
-  const updatedSubrepo = await getSubrepo(monorepoDir, subrepoSubdir)
-  const updatedSubrepoCommit = updatedSubrepo['pulled-commit']
-
-  if (subrepoCommit !== updatedSubrepoCommit) {
-    subrepoCommit = updatedSubrepoCommit
-
-    // The secretUrl gets recorded in git-subrepo config file, so reset to the commit before.
-    // Intended for CI. If happens in a developer's repo, history will be messed up a bit
-    // but better that than accidentally committing the token to the codebase later.
-    if (secretUrl) {
-      await execLive(['git', 'reset', '--hard', 'HEAD~1'])
-    }
-  }
-
-  const tagNameShort = `v${version}`
-  const tagName = `subrepo/${subrepoSubdir}/${tagNameShort}`
-
-  console.log(`Creating tag ${tagName} ...`)
-  await execLive(['git', 'tag', '-a', tagName, '-m', tagNameShort, subrepoCommit], execOpts)
+  console.log(`Creating temp tag ${tempTagName} ...`)
+  await execLive(['git', 'tag', '-a', tempTagName, '-m', tagName, subrepoCommit], execOpts)
 
   try {
-    console.log(`Pushing tag ${tagName} ...`)
-    await execLive(
-      // provide a tag refspec so the remote gets the 'short' name
-      ['git', 'push', secretUrl || subrepoRemote, `${tagName}:${tagNameShort}`],
-      execOpts,
-    )
+    console.log(`Pushing as tag ${tagName} ...`)
+    await execLive(['git', 'push', subrepoRemote, `${tempTagName}:${tagName}`], execOpts)
   } finally {
-    console.log(`Locally deleting tag ${tagName} ...`)
-    await execLive(['git', 'tag', '-d', tagName], execOpts)
+    console.log(`Deleting temp tag ${tempTagName} ...`)
+    await execLive(['git', 'tag', '-d', tempTagName], execOpts)
   }
 
-  // Create GitHub release
-  if (githubToken) {
-    const isStandard = githubRepo === 'fullcalendar/fullcalendar' // TODO: read from manifest?
-    await createGithubRelease(
-      githubToken,
-      githubRepo,
-      tagNameShort, // the tag the remote uses
-      version,
-      monorepoDir,
-      isStandard && `standard/dist/fullcalendar-${version}.zip`,
-      !isStandard, // linkToStandard
-    )
-  } else if (!isCi) {
-    console.log('Skipping release creation')
-  } else {
-    throw new Error(`Cannot create release without ${githubTokenName}`)
-  }
+  await createGithubRelease(
+    remoteToGithubRepo(subrepoRemote),
+    tagName, // the tag the remote uses
+    version,
+    monorepoDir,
+    subrepoSubdir === 'standard' && `standard/dist/fullcalendar-${version}.zip`,
+    subrepoSubdir !== 'standard', // linkToStandard
+  )
 }
 
 async function createGithubRelease(
-  githubToken: string,
   githubRepo: string,
   tagName: string,
   version: string,
@@ -176,38 +102,20 @@ async function createGithubRelease(
   filePath?: string | false,
   linkToStandard?: boolean,
 ): Promise<void> {
-  const octokit = new Octokit({ auth: githubToken })
-  const octokitHeaders = { 'X-GitHub-Api-Version': '2022-11-28' }
-
-  const [owner, repo] = githubRepo.split('/')
+  const execOpts = { cwd: monorepoDir }
   const body = linkToStandard
     ? `See https://github.com/fullcalendar/fullcalendar/releases/tag/${tagName}`
     : (await getChangelogEntry(joinPaths(monorepoDir, changelogSrc), version) ||
         '_Manually enter release notes_')
 
-  console.log(`Creating release in ${githubRepo} ...`)
-  const releaseRes = await octokit.request('POST /repos/{owner}/{repo}/releases', {
-    owner,
-    repo,
-    tag_name: tagName,
-    prerelease: tagName.includes('-'),
-    body,
-    headers: octokitHeaders,
-  })
-
-  if (filePath) {
-    const fileName = basename(filePath)
-    const fileRaw = await readFile(joinPaths(monorepoDir, filePath))
-
-    console.log(`Uploading release asset ${fileName} ...`)
-    await octokit.request({
-      method: 'POST',
-      url: releaseRes.data.upload_url,
-      name: fileName,
-      data: fileRaw,
-      headers: octokitHeaders,
-    })
-  }
+  await execLive([
+    'gh', 'release', 'create',
+    '--repo', githubRepo,
+    ...(tagName.includes('-') ? ['--prerelease'] : []),
+    '--note', body,
+    tagName,
+    ...(filePath ? [joinPaths(monorepoDir, filePath)] : []),
+  ], execOpts)
 }
 
 function remoteToGithubRepo(remote: string): string {
