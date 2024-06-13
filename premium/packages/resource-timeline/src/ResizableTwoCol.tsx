d64c2f4d963f8faa012c47945f85b8a2db346bc2
Adam Shaw <arshaw@users.noreply.github.com>
2024-06-13 18:35:26 -0400

    forgot some files

---
+++ premium/packages/resource-timeline/src/ResizableTwoCol.tsx
@@ -0,0 +1,87 @@
+import { BaseComponent, ElementDragging, PointerDragEvent } from '@fullcalendar/core/internal';
+import { ComponentChildren, createElement, createRef } from '@fullcalendar/core/preact';
+
+export interface ResizableTwoColProps {
+  startContent: ComponentChildren
+  endContent: ComponentChildren
+}
+
+interface ResizableTwoColState {
+  startWidthOverride: number | undefined
+}
+
+const MIN_RESOURCE_AREA_WIDTH = 30 // definitely bigger than scrollbars
+
+export class ResizableTwoCol extends BaseComponent<ResizableTwoColProps, ResizableTwoColState> {
+  rootElRef = createRef<HTMLDivElement>()
+  resizerElRef = createRef<HTMLDivElement>()
+  resizerDragging: ElementDragging
+
+  render() {
+    let { props, state, context } = this
+    let { options, theme } = context
+
+    let resourceAreaWidth = state.startWidthOverride != null
+      ? state.startWidthOverride
+      : options.resourceAreaWidth
+
+    return (
+      <div ref={this.rootElRef}>
+        <div style={{ width: resourceAreaWidth }}>
+          {props.startContent}
+        </div>
+        <div
+          ref={this.resizerElRef}
+          className={'fc-resource-timeline-divider ' + theme.getClass('tableCellShaded')}
+        />
+        <div>
+          {props.endContent}
+        </div>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    this.initResizing()
+  }
+
+  componentWillUnmount() {
+    this.destroyResizing()
+  }
+
+  initResizing() {
+    let { isRtl, pluginHooks } = this.context
+    let ElementDraggingImpl = pluginHooks.elementDraggingImpl
+    let resizerEl = this.resizerElRef.current
+
+    if (ElementDraggingImpl) {
+      let rootEl = this.rootElRef.current
+      let dragging = this.resizerDragging = new ElementDraggingImpl(resizerEl)
+      let dragStartWidth
+      let viewWidth
+
+      dragging.emitter.on('dragstart', () => {
+        dragStartWidth = resizerEl.getBoundingClientRect().width
+        viewWidth = rootEl.getBoundingClientRect().width
+      })
+
+      dragging.emitter.on('dragmove', (pev: PointerDragEvent) => {
+        let newWidth = dragStartWidth + pev.deltaX * (isRtl ? -1 : 1)
+        newWidth = Math.max(newWidth, MIN_RESOURCE_AREA_WIDTH)
+        newWidth = Math.min(newWidth, viewWidth - MIN_RESOURCE_AREA_WIDTH)
+
+        this.setState({
+          startWidthOverride: newWidth,
+        })
+      })
+
+      dragging.setAutoScrollEnabled(false) // because gets weird with auto-scrolling time area
+    }
+  }
+
+  destroyResizing() {
+    if (this.resizerDragging) {
+      this.resizerDragging.destroy()
+    }
+  }
+}
---
+++ premium/packages/resource/src/common/ResourceDayHeaderCell.tsx
@@ -0,0 +1,42 @@
+import { BaseComponent, DateMarker } from '@fullcalendar/core/internal'
+import { createElement } from '@fullcalendar/core/preact'
+import { Resource } from '../structs/resource.js'
+import { ResourceLabelContainer } from './ResourceLabelContainer.js'
+
+export interface ResourceDayHeaderCellProps {
+  resource: Resource
+  colSpan: number
+  date?: DateMarker
+  isSticky?: boolean
+}
+
+export class ResourceDayHeaderCell extends BaseComponent<ResourceDayHeaderCellProps> {
+  render() {
+    let { props } = this
+
+    return (
+      <ResourceLabelContainer
+        elTag="th"
+        elClasses={['fc-col-header-cell', 'fc-resource']}
+        elAttrs={{
+          role: 'columnheader',
+          colSpan: props.colSpan,
+        }}
+        resource={props.resource}
+        date={props.date}
+      >
+        {(InnerContent) => (
+          <div className="fc-scrollgrid-sync-inner">
+            <InnerContent
+              elTag="span"
+              elClasses={[
+                'fc-col-header-cell-cushion',
+                props.isSticky && 'fc-sticky',
+              ]}
+            />
+          </div>
+        )}
+      </ResourceLabelContainer>
+    )
+  }
+}
