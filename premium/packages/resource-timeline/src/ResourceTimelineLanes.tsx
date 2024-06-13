3c53ae754cfaeb2be6c385166953f59676f46fa1
Adam Shaw <arshaw@users.noreply.github.com>
2024-06-13 12:05:11 -0400

    remove ResourceTimelineLanesBody

--- premium/packages/resource-timeline/src/ResourceTimelineLanes.tsx
+++ premium/packages/resource-timeline/src/ResourceTimelineLanes.tsx
@@ -1,9 +1,27 @@
 import { CssDimValue } from '@fullcalendar/core'
-import { BaseComponent } from '@fullcalendar/core/internal'
-import { createElement } from '@fullcalendar/core/preact'
-import { ResourceTimelineLanesBody, ResourceTimelineLanesContentProps } from './ResourceTimelineLanesBody.js'
+import {
+  SplittableProps, EventStore, BaseComponent,
+  DateMarker, DateRange, DateProfile,
+} from '@fullcalendar/core/internal'
+import { Fragment, createElement } from '@fullcalendar/core/preact'
+import { Group, Resource } from '@fullcalendar/resource/internal'
+import { TimelineDateProfile, TimelineCoords } from '@fullcalendar/timeline/internal'
+import { ResourceTimelineLane } from './ResourceTimelineLane.js'
+import { DividerRow } from './DividerRow.js'
+import { GroupRowDisplay, ResourceRowDisplay } from './resource-table.js'
 
-export interface ResourceTimelineLanesProps extends ResourceTimelineLanesContentProps {
+export interface ResourceTimelineLanesProps {
+  groupRowDisplays: GroupRowDisplay[]
+  resourceRowDisplays: ResourceRowDisplay[]
+  splitProps: { [resourceId: string]: SplittableProps }
+  dateProfile: DateProfile
+  tDateProfile: TimelineDateProfile
+  nowDate: DateMarker
+  todayRange: DateRange
+  fallbackBusinessHours: EventStore | null
+  slatCoords: TimelineCoords | null
+  verticalPositions: Map<Resource | Group, { top: number, height: number }>
+  //
   minHeight: CssDimValue
   clientWidth: number | null
   tableMinWidth: CssDimValue
@@ -23,18 +41,39 @@ export class ResourceTimelineLanes extends BaseComponent<ResourceTimelineLanesPr
           height: props.minHeight,
         }}
       >
-        <ResourceTimelineLanesBody
-          groupRowDisplays={props.groupRowDisplays}
-          resourceRowDisplays={props.resourceRowDisplays}
-          dateProfile={props.dateProfile}
-          tDateProfile={props.tDateProfile}
-          nowDate={props.nowDate}
-          todayRange={props.todayRange}
-          splitProps={props.splitProps}
-          fallbackBusinessHours={props.fallbackBusinessHours}
-          slatCoords={props.slatCoords}
-          verticalPositions={props.verticalPositions}
-        />
+        <tbody>
+          <Fragment>
+            {props.groupRowDisplays.map((groupRowDisplay) => (
+              <DividerRow
+                key={String(groupRowDisplay.group.value)}
+                group={groupRowDisplay.group}
+                top={props.verticalPositions.get(groupRowDisplay.group).top}
+                height={props.verticalPositions.get(groupRowDisplay.group).height}
+              />
+            ))}
+          </Fragment>
+          <Fragment>
+            {props.resourceRowDisplays.map((resourceRowDisplay) => {
+              const { resource } = resourceRowDisplay
+              return (
+                <ResourceTimelineLane
+                  key={resource.id}
+                  {...props.splitProps[resource.id]}
+                  resource={resource}
+                  dateProfile={props.dateProfile}
+                  tDateProfile={props.tDateProfile}
+                  nowDate={props.nowDate}
+                  todayRange={props.todayRange}
+                  nextDayThreshold={context.options.nextDayThreshold}
+                  businessHours={resource.businessHours || props.fallbackBusinessHours}
+                  timelineCoords={props.slatCoords}
+                  top={props.verticalPositions.get(resource).top}
+                  height={props.verticalPositions.get(resource).height}
+                />
+              )
+            })}
+          </Fragment>
+        </tbody>
       </table>
     )
   }
