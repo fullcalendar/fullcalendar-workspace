b1d9aacc926e550e79901b5f4b4ba4b4279ded2d
Adam Shaw <arshaw@users.noreply.github.com>
2024-09-18 12:50:24 -0400

    strip out outdated print mode

--- premium/packages/adaptive/src/global-handlers.ts
+++ premium/packages/adaptive/src/global-handlers.ts
@@ -1,15 +1,6 @@
-import {
-  findElements,
-  CalendarContext,
-  removeExact,
-  config,
-} from '@fullcalendar/core/internal'
-import { flushSync } from '@fullcalendar/core/preact'
-
-config.COLLAPSIBLE_WIDTH_THRESHOLD = 1200
+import { CalendarContext, removeExact } from '@fullcalendar/core/internal'
 
 let contexts: CalendarContext[] = []
-let undoFuncs: (() => void)[] = []
 
 export function contextInit(context: CalendarContext): void {
   if (!contexts.length) {
@@ -51,42 +42,10 @@ function handleBeforePrint() {
   for (let context of contexts) {
     context.emitter.trigger('_beforeprint')
   }
-
-  flushSync(() => { // because printing grabs DOM immediately after
-    undoFuncs.push(freezeScrollgridWidths())
-  })
 }
 
 function handleAfterPrint() {
   for (let context of contexts) {
     context.emitter.trigger('_afterprint')
   }
-
-  flushSync(() => { // guarantee that there are real scrollers
-    while (undoFuncs.length) {
-      undoFuncs.shift()()
-    }
-  })
-}
-
-// scrollgrid widths
-// TODO: kill this stuff!!!
-
-function freezeScrollgridWidths() {
-  let els = findElements(document.body, '.fcnew-scrollgrid')
-  els.forEach(freezeScrollGridWidth)
-  return () => els.forEach(unfreezeScrollGridWidth)
-}
-
-function freezeScrollGridWidth(el: HTMLElement) {
-  let elWidth = el.getBoundingClientRect().width
-
-  // along with collapsibleWidth, this is a hack for #5707
-  if (!el.classList.contains('fcnew-scrollgrid-collapsible') || elWidth < config.COLLAPSIBLE_WIDTH_THRESHOLD) {
-    el.style.width = elWidth + 'px'
-  }
-}
-
-function unfreezeScrollGridWidth(el) {
-  el.style.width = ''
 }
