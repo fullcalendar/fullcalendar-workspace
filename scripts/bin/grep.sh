cc536e702cbe6f7d3892812e2fe8e00633ae81f8
Adam Shaw <arshaw@users.noreply.github.com>
2024-09-26 00:05:41 -0400

    improve grep

--- scripts/bin/grep.sh
+++ scripts/bin/grep.sh
@@ -3,4 +3,12 @@
 set -e
 cd "`dirname $0`/../.."
 
-grep -n --color=auto -r -E $1 */packages/*/src
+q=$1
+
+if [ -z "$q" ]; then
+  echo "Error: No search query provided"
+  exit 1
+fi
+
+grep -n --color=auto -r -E "$q" */packages/*/src || true
+grep -n --color=auto -r -E "$q" */tests/src || true
