a19b836ac5b49756d84e726634e4032c040cab0f
Adam Shaw <arshaw@users.noreply.github.com>
2024-12-05 16:02:45 -0500

    kill safeSetState

--- standard/packages/core/src/component-util/rendering-misc.ts
+++ standard/packages/core/src/component-util/rendering-misc.ts
@@ -1,6 +1,4 @@
 
-// TODO: kill Component::safeSetState
-
 export function isDimsEqual(v0: number | undefined, v1: number): boolean {
   return v0 != null && (v0 === v1 || Math.abs(v0 - v1) < 0.01)
 }
