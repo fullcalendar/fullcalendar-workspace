27341df3883dff2409890069290ddc0ccabc1515
Adam Shaw <arshaw@users.noreply.github.com>
2025-04-17 21:59:20 -0400

    update theming demos

--- standard/tests/manual/js/theme-chooser.js
+++ standard/tests/manual/js/theme-chooser.js
@@ -1,67 +1,20 @@
 
-function initThemeChooser(settings) {
-  var isInitialized = false
-  var currentThemeSystem // don't set this directly. use setThemeSystem
+function initThemeChooser() {
   var currentStylesheetEl
   var loadingEl = document.getElementById('loading')
-  var systemSelectEl = document.querySelector('#theme-system-selector select')
-  var themeSelectWrapEls = Array.prototype.slice.call( // convert to real array
-    document.querySelectorAll('.selector[data-theme-system]'),
-  )
+  var themeSelectEl = document.querySelector('.selector select')
 
-  systemSelectEl.addEventListener('change', function() {
-    setThemeSystem(this.value)
+  setTheme(themeSelectEl.value)
+  themeSelectEl.addEventListener('change', function() {
+    setTheme(themeSelectEl.value)
   })
 
-  setThemeSystem(systemSelectEl.value)
-
-  themeSelectWrapEls.forEach(function(themeSelectWrapEl) {
-    var themeSelectEl = themeSelectWrapEl.querySelector('select')
-
-    themeSelectWrapEl.addEventListener('change', function() {
-      setTheme(
-        currentThemeSystem,
-        themeSelectEl.options[themeSelectEl.selectedIndex].value,
-      )
-    })
-  })
-
-
-  function setThemeSystem(themeSystem) {
-    var selectedTheme
-
-    currentThemeSystem = themeSystem
-
-    themeSelectWrapEls.forEach(function(themeSelectWrapEl) {
-      var themeSelectEl = themeSelectWrapEl.querySelector('select')
-      var themeSystems = (themeSelectWrapEl.getAttribute('data-theme-system') || '').split(',')
-
-      if (themeSystems.includes(themeSystem)) {
-        selectedTheme = themeSelectEl.options[themeSelectEl.selectedIndex].value
-        themeSelectWrapEl.style.display = 'inline-block'
-      } else {
-        themeSelectWrapEl.style.display = 'none'
-      }
-    })
-
-    setTheme(themeSystem, selectedTheme)
-  }
-
-
-  function setTheme(themeSystem, themeName) {
-    var stylesheetUrl = generateStylesheetUrl(themeSystem, themeName)
+  function setTheme(themeName) {
+    var stylesheetUrl = generateStylesheetUrl(themeName)
     var stylesheetEl
 
     function done() {
-      if (!isInitialized) {
-        isInitialized = true
-        settings.init(themeSystem)
-      }
-      else {
-        settings.change(themeSystem)
-      }
-
-      showCredits(themeSystem, themeName)
+      showCredits(themeName)
     }
 
     if (stylesheetUrl) {
@@ -89,37 +42,16 @@ function initThemeChooser(settings) {
     }
   }
 
-
-  function generateStylesheetUrl(themeSystem, themeName) {
-    if (themeSystem === 'bootstrap') {
-      if (themeName) {
-        return 'https://bootswatch.com/4/' + themeName + '/bootstrap.min.css'
-      }
-      else { // the default bootstrap theme
-        return 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
-      }
-    } else if (themeSystem === 'bootstrap5') {
-      if (themeName) {
-        return 'https://bootswatch.com/5/' + themeName + '/bootstrap.min.css'
-      }
-      else { // the default bootstrap theme
-        return 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
-      }
+  function generateStylesheetUrl(themeName) {
+    if (themeName) {
+      return 'https://bootswatch.com/5/' + themeName + '/bootstrap.min.css'
+    } else { // the default bootstrap theme
+      return 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css'
     }
   }
 
-
-  function showCredits(themeSystem, themeName) {
-    var creditId
-
-    if (themeSystem.match('bootstrap')) {
-      if (themeName) {
-        creditId = 'bootstrap-custom'
-      }
-      else {
-        creditId = 'bootstrap-standard'
-      }
-    }
+  function showCredits(themeName) {
+    var creditId = 'bootstrap' + (themeName ? '-custom' : '')
 
     Array.prototype.slice.call( // convert to real array
       document.querySelectorAll('.credits'),
@@ -131,7 +63,6 @@ function initThemeChooser(settings) {
       }
     })
   }
-
 
   function whenStylesheetLoaded(linkNode, callback) {
     var isReady = false
