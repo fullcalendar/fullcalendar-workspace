92c402826b973f48ccc523bd93cc2046d73ddad8
Adam Shaw <arshaw@users.noreply.github.com>
2025-04-25 16:57:40 -0400

    move adaptive css into core/theme

--- premium/packages/adaptive/src/index.ts
+++ premium/packages/adaptive/src/index.ts
@@ -2,7 +2,6 @@ import { createPlugin, PluginDef } from '@fullcalendar/core'
 import premiumCommonPlugin from '@fullcalendar/premium-common'
 import { contextInit } from './global-handlers.js'
 import './ambient.js'
-import './index.css'
 
 export default createPlugin({
   name: '<%= pkgName %>',
