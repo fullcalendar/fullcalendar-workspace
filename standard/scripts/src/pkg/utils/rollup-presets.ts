eb9e6d8faa22270d8ef8e8d71035def6f2a03e16
Adam Shaw <arshaw@users.noreply.github.com>
2025-05-03 14:15:29 -0400

    terse obfuscated classnames

--- standard/scripts/src/pkg/utils/rollup-presets.ts
+++ standard/scripts/src/pkg/utils/rollup-presets.ts
@@ -1,5 +1,6 @@
 import { readFile } from 'fs/promises'
 import { join as joinPaths } from 'path'
+import { createHash } from 'crypto'
 import { RollupOptions, Plugin, OutputOptions, RollupWarning } from 'rollup'
 import handlebars from 'handlebars'
 import nodeResolvePlugin from '@rollup/plugin-node-resolve'
@@ -391,7 +392,9 @@ function cssPlugin(options?: { inject?: CssInjector | boolean }): Plugin {
       ctx: {}, // arguments given to config file
     },
     modules: {
-      generateScopedName: 'fc-[local]__[hash:base64:5]',
+      generateScopedName(localName: string, resourcePath: string) {
+        return 'fc-' + hashGenerator.generate(localName + resourcePath)
+      },
     },
     inject: typeof inject === 'object' ?
       (cssVarName: string) => {
@@ -442,3 +445,63 @@ function onwarn(warning: RollupWarning) {
 function cjsInterop<DefaultExport>(namespace: { default: DefaultExport }): DefaultExport {
   return namespace.default || (namespace as DefaultExport)
 }
+
+// Crypto
+// -------------------------------------------------------------------------------------------------
+
+class HashGenerator {
+  private hashMap: Map<string, string> = new Map()
+  private usedHashes: Set<string> = new Set()
+
+  constructor(private hashLength: number, private salt: string) {
+  }
+
+  generate(input: string) {
+    if (this.hashMap.has(input)) {
+      return this.hashMap.get(input)
+    }
+
+    const hash = this.generateNew(input)
+    this.hashMap.set(input, hash)
+    return hash
+  }
+
+  generateNew(input: string) {
+    let attemptCnt = 0
+    let hash
+
+    do {
+      hash = this.generateAttempt(input, attemptCnt++)
+    } while (this.usedHashes.has(hash));
+
+    this.usedHashes.add(hash)
+    return hash
+  }
+
+  generateAttempt(input: string, attemptNum: number) {
+    if (attemptNum > 100) {
+      throw new Error('Too many attempts. Consider increasing hashLength')
+    }
+
+    return hashToAlphaNumeric(String(attemptNum) + this.salt + input)
+      .substring(0, this.hashLength)
+  }
+}
+
+const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+function hashToAlphaNumeric(input: string) {
+  const hash = createHash('md5').update(input).digest() // Buffer of 16 bytes
+  let num = BigInt('0x' + hash.toString('hex')) // Convert to BigInt
+
+  // Convert to base62
+  let result = ''
+  while (num > 0n) {
+    result += CHARS[Number(num % 62n)]
+    num = num / 62n
+  }
+
+  return result
+}
+
+const hashGenerator = new HashGenerator(2, 'adamiscool')
