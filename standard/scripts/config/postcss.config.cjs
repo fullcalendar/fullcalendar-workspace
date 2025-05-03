f987f0ae96bfc31a9e56457072d1879738bdf99f
Adam Shaw <arshaw@users.noreply.github.com>
2025-05-03 13:04:49 -0400

    remove postcss plugins

--- standard/scripts/config/postcss.config.cjs
+++ standard/scripts/config/postcss.config.cjs
@@ -3,11 +3,7 @@ NOTE: unfortunately can't rename this file to `postcss.cjs` or error is thrown
 */
 
 module.exports = {
-  parser: require('postcss-comment'), // for "//" style comments
   plugins: [
-    // TODO: audit these
-    require('postcss-advanced-variables'),
-    require('postcss-nesting'),
     require('autoprefixer'),
   ],
 }
