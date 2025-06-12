827b5bf2b747f61cb631970b6b0e5cdf1306cf90
Adam Shaw <arshaw@users.noreply.github.com>
2025-06-12 18:46:00 -0400

    testing for other premium plugins

--- examples/classic-theme-tailwind/src/main.ts
+++ examples/classic-theme-tailwind/src/main.ts
@@ -8,7 +8,9 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import multiMonthPlugin from '@fullcalendar/multimonth'
 import interactionPlugin from '@fullcalendar/interaction'
+import resourceDayGridPlugin from '@fullcalendar/resource-daygrid'
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
 import scrollGridPlugin from '@fullcalendar/scrollgrid'
 import timelinePlugin from '@fullcalendar/timeline'
 import themePlugin from './theme.js'
@@ -146,11 +148,15 @@ document.addEventListener('DOMContentLoaded', function() {
         adaptivePlugin,
         timelinePlugin,
         resourceTimelinePlugin,
+        resourceTimeGridPlugin,
+        resourceDayGridPlugin,
+        scrollGridPlugin,
         interactionPlugin,
         themePlugin,
       ],
       initialDate: '2023-01-07',
-      initialView: 'resourceTimelineThreeDays',
+      initialView: 'resourceTimeGridWeek',
+      dayMinWidth: 200,
       editable: true,
       selectable: true,
       nowIndicator: true,
@@ -194,32 +200,32 @@ document.addEventListener('DOMContentLoaded', function() {
         { id: 'a', building: '460 Bryant', title: 'Auditorium A', occupancy: 40 },
         { id: 'b', building: '460 Bryant', title: 'Auditorium B', occupancy: 40, eventColor: 'green' },
         { id: 'c', building: '460 Bryant', title: 'Auditorium C', occupancy: 40, eventColor: 'orange' },
-        { id: 'd', building: '460 Bryant', title: 'Auditorium D', occupancy: 40, children: [
-          { id: 'd1', title: 'Room D1', occupancy: 10 },
-          { id: 'd2', title: 'Room D2', occupancy: 10 }
-        ] },
-        { id: 'e', building: '460 Bryant', title: 'Auditorium E', occupancy: 40 },
-        { id: 'f', building: '460 Bryant', title: 'Auditorium F', occupancy: 40, eventColor: 'red' },
-        { id: 'g', building: '564 Pacific', title: 'Auditorium G', occupancy: 40 },
-        { id: 'h', building: '564 Pacific', title: 'Auditorium H', occupancy: 40 },
-        { id: 'i', building: '564 Pacific', title: 'Auditorium I', occupancy: 40 },
-        { id: 'j', building: '564 Pacific', title: 'Auditorium J', occupancy: 40 },
-        { id: 'k', building: '564 Pacific', title: 'Auditorium K', occupancy: 40 },
-        { id: 'l', building: '564 Pacific', title: 'Auditorium L', occupancy: 40 },
-        { id: 'm', building: '564 Pacific', title: 'Auditorium M', occupancy: 40 },
-        { id: 'n', building: '564 Pacific', title: 'Auditorium N', occupancy: 40 },
-        { id: 'o', building: '564 Pacific', title: 'Auditorium O', occupancy: 40 },
-        { id: 'p', building: '564 Pacific', title: 'Auditorium P', occupancy: 40 },
-        { id: 'q', building: '564 Pacific', title: 'Auditorium Q', occupancy: 40 },
-        { id: 'r', building: '564 Pacific', title: 'Auditorium R', occupancy: 40 },
-        { id: 's', building: '564 Pacific', title: 'Auditorium S', occupancy: 40 },
-        { id: 't', building: '564 Pacific', title: 'Auditorium T', occupancy: 40 },
-        { id: 'u', building: '564 Pacific', title: 'Auditorium U', occupancy: 40 },
-        { id: 'v', building: '564 Pacific', title: 'Auditorium V', occupancy: 40 },
-        { id: 'w', building: '564 Pacific', title: 'Auditorium W', occupancy: 40 },
-        { id: 'x', building: '564 Pacific', title: 'Auditorium X', occupancy: 40 },
-        { id: 'y', building: '564 Pacific', title: 'Auditorium Y', occupancy: 40 },
-        { id: 'z', building: '564 Pacific', title: 'Auditorium Z', occupancy: 40 }
+        // { id: 'd', building: '460 Bryant', title: 'Auditorium D', occupancy: 40, children: [
+        //   { id: 'd1', title: 'Room D1', occupancy: 10 },
+        //   { id: 'd2', title: 'Room D2', occupancy: 10 }
+        // ] },
+        // { id: 'e', building: '460 Bryant', title: 'Auditorium E', occupancy: 40 },
+        // { id: 'f', building: '460 Bryant', title: 'Auditorium F', occupancy: 40, eventColor: 'red' },
+        // { id: 'g', building: '564 Pacific', title: 'Auditorium G', occupancy: 40 },
+        // { id: 'h', building: '564 Pacific', title: 'Auditorium H', occupancy: 40 },
+        // { id: 'i', building: '564 Pacific', title: 'Auditorium I', occupancy: 40 },
+        // { id: 'j', building: '564 Pacific', title: 'Auditorium J', occupancy: 40 },
+        // { id: 'k', building: '564 Pacific', title: 'Auditorium K', occupancy: 40 },
+        // { id: 'l', building: '564 Pacific', title: 'Auditorium L', occupancy: 40 },
+        // { id: 'm', building: '564 Pacific', title: 'Auditorium M', occupancy: 40 },
+        // { id: 'n', building: '564 Pacific', title: 'Auditorium N', occupancy: 40 },
+        // { id: 'o', building: '564 Pacific', title: 'Auditorium O', occupancy: 40 },
+        // { id: 'p', building: '564 Pacific', title: 'Auditorium P', occupancy: 40 },
+        // { id: 'q', building: '564 Pacific', title: 'Auditorium Q', occupancy: 40 },
+        // { id: 'r', building: '564 Pacific', title: 'Auditorium R', occupancy: 40 },
+        // { id: 's', building: '564 Pacific', title: 'Auditorium S', occupancy: 40 },
+        // { id: 't', building: '564 Pacific', title: 'Auditorium T', occupancy: 40 },
+        // { id: 'u', building: '564 Pacific', title: 'Auditorium U', occupancy: 40 },
+        // { id: 'v', building: '564 Pacific', title: 'Auditorium V', occupancy: 40 },
+        // { id: 'w', building: '564 Pacific', title: 'Auditorium W', occupancy: 40 },
+        // { id: 'x', building: '564 Pacific', title: 'Auditorium X', occupancy: 40 },
+        // { id: 'y', building: '564 Pacific', title: 'Auditorium Y', occupancy: 40 },
+        // { id: 'z', building: '564 Pacific', title: 'Auditorium Z', occupancy: 40 }
       ],
       events: [
         { id: '1', resourceId: 'b', start: '2023-01-07T02:00:00', end: '2023-01-07T07:00:00', title: 'event 1' },
