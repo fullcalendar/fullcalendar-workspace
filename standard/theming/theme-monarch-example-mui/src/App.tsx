3f178f9884bdeab14884d2aad5925d81096e089c
Adam Shaw <arshaw@users.noreply.github.com>
2025-06-28 12:58:46 -0400

    use fullcalendar in mui

--- standard/theming/theme-monarch-example-mui/src/App.tsx
+++ standard/theming/theme-monarch-example-mui/src/App.tsx
@@ -1,19 +1,196 @@
-import { useState } from 'react'
-import Button from '@mui/material/Button'
 import './App.css'
+import Button from '@mui/material/Button'
+
+import '@fullcalendar/core/global.css'
+import FullCalendar, { useCalendarController } from '@fullcalendar/react'
+import adaptivePlugin from '@fullcalendar/adaptive'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+import multiMonthPlugin from '@fullcalendar/multimonth'
+import interactionPlugin from '@fullcalendar/interaction'
+import resourceDayGridPlugin from '@fullcalendar/resource-daygrid'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
+import scrollGridPlugin from '@fullcalendar/scrollgrid'
+import timelinePlugin from '@fullcalendar/timeline'
+import themePlugin from '@fullcalendar/theme-monarch/mui'
+
+// TODO: kill reliance on --fc-canvas-color somehow
+// TODO: aria-labels not working, esp for "Today"
+// TODO: borderless must kill border-radius and drop-shadow!
+
+const enablePremium = false
+const enableDark = false
+
+if (enableDark) {
+  document.documentElement.classList.add('dark')
+}
+
+// // TODO: make different for premium
+// const availableViews = [
+//   'dayGridMonth',
+//   'timeGridWeek',
+//   'timeGridDay',
+//   'listWeek',
+//   'multiMonthYear',
+// ]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const controller = useCalendarController()
+  // const buttons = controller.getButtonState()
 
   return (
-    <div>
-      <Button
-        onClick={() => setCount((count) => count + 1)}
-        variant="contained"
-      >count is {count}</Button>
-      <div className='p-10 bg-gray-500'>
-        Hello World
+    <div className='my-10 max-w-[1100px] mx-auto gap-5 border rounded-xl'>
+      <div className='flex items-center p-3 justify-between'>
+        <div className='flex items-center gap-2'>
+          <Button variant="contained">Hello World</Button>
+        </div>
       </div>
+      {!enablePremium ? (
+        <FullCalendar
+          controller={controller}
+          weekNumbers={true}
+          plugins={[
+            scrollGridPlugin,
+            adaptivePlugin,
+            dayGridPlugin,
+            timeGridPlugin,
+            listPlugin,
+            interactionPlugin,
+            multiMonthPlugin,
+            themePlugin,
+          ]}
+          eventInteractive={true}
+          initialDate='2023-01-12'
+          initialView='dayGridMonth'
+          nowIndicator={true}
+          borderless={true}
+          headerToolbar={false}
+          navLinks={true}
+          editable={true}
+          selectable={true}
+          selectMirror={false}
+          dayMaxEvents={true}
+          // businessHours={true} // -- TODO: background conflicts with the week number pills!!!
+          eventMaxStack={1}
+          listDayFormat={{ day: 'numeric' }}
+          listDaySideFormat={{ month: 'short', weekday: 'short', forceCommas: true }}
+          views={{
+            timeGrid: {
+              slotDuration: '01:00',
+            },
+          }}
+          events={[
+            { title: 'All Day Event', start: '2023-01-01' },
+            { title: 'Long Event', start: '2023-01-07', end: '2023-01-10', color: 'red' },
+            { groupId: '999', title: 'Repeating Event', start: '2023-01-09T16:00:00' },
+            { groupId: '999', title: 'Repeating Event', start: '2023-01-16T16:00:00' },
+            { title: 'Conference', start: '2023-01-11', end: '2023-01-13', display: 'background' },
+            { title: 'Meeting', start: '2023-01-12T12:00:00' },
+            { title: 'Lunch', start: '2023-01-12T12:00:00' },
+            { title: 'Meeting', start: '2023-01-12T14:30:00' },
+            { title: 'Happy Hour', start: '2023-01-12T17:30:00' },
+            { title: 'Dinner', start: '2023-01-12T20:00:00' },
+            { title: 'Birthday Party', start: '2023-01-13T07:00:00' },
+            { title: 'Click for Google', url: 'http://google.com/', start: '2023-01-28' },
+          ]}
+        />
+      ) : (
+        <FullCalendar
+          plugins={[
+            adaptivePlugin,
+            timelinePlugin,
+            resourceTimelinePlugin,
+            resourceTimeGridPlugin,
+            resourceDayGridPlugin,
+            scrollGridPlugin,
+            interactionPlugin,
+            themePlugin,
+          ]}
+          initialDate='2023-01-07'
+          initialView='resourceTimelineThreeDays'
+          dayMinWidth={200}
+          editable={true}
+          selectable={true}
+          nowIndicator={true}
+          aspectRatio={1.8}
+          scrollTime='00:00'
+          headerToolbar={{
+            left: 'today prev,next',
+            center: 'title',
+            right: 'resourceTimelineDay,resourceTimelineThreeDays',
+          }}
+          buttons={{
+            resourceTimelineThreeDays: {
+              text: '3 days',
+            }
+          }}
+          views={{
+            resourceTimelineThreeDays: {
+              type: 'resourceTimeline',
+              duration: { days: 3 },
+            },
+            resourceTimeline: {
+              slotDuration: '01:00',
+              snapDuration: '00:30',
+            },
+          }}
+          resourceAreaHeaderContent='yooo'
+          resourceAreaWidth='40%'
+          resourceGroupField='building'
+          resourceAreaColumns={[
+            { headerContent: 'Building', field: 'building' },
+            { headerContent: 'Room', field: 'title' },
+            { headerContent: 'Occupancy', field: 'occupancy' },
+          ]}
+          resources={[
+            { id: 'a', building: '460 Bryant', title: 'Auditorium A', occupancy: 40 },
+            { id: 'b', building: '460 Bryant', title: 'Auditorium B', occupancy: 40, eventColor: 'green' },
+            { id: 'c', building: '460 Bryant', title: 'Auditorium C', occupancy: 40, eventColor: 'orange' },
+            {
+              id: 'd',
+              building: '460 Bryant',
+              title: 'Auditorium D',
+              occupancy: 40,
+              children: [
+                { id: 'd1', title: 'Room D1', occupancy: 10 },
+                { id: 'd2', title: 'Room D2', occupancy: 10 },
+              ],
+            },
+            { id: 'e', building: '460 Bryant', title: 'Auditorium E', occupancy: 40 },
+            { id: 'f', building: '460 Bryant', title: 'Auditorium F', occupancy: 40, eventColor: 'red' },
+            { id: 'g', building: '564 Pacific', title: 'Auditorium G', occupancy: 40 },
+            { id: 'h', building: '564 Pacific', title: 'Auditorium H', occupancy: 40 },
+            { id: 'i', building: '564 Pacific', title: 'Auditorium I', occupancy: 40 },
+            { id: 'j', building: '564 Pacific', title: 'Auditorium J', occupancy: 40 },
+            { id: 'k', building: '564 Pacific', title: 'Auditorium K', occupancy: 40 },
+            { id: 'l', building: '564 Pacific', title: 'Auditorium L', occupancy: 40 },
+            { id: 'm', building: '564 Pacific', title: 'Auditorium M', occupancy: 40 },
+            { id: 'n', building: '564 Pacific', title: 'Auditorium N', occupancy: 40 },
+            { id: 'o', building: '564 Pacific', title: 'Auditorium O', occupancy: 40 },
+            { id: 'p', building: '564 Pacific', title: 'Auditorium P', occupancy: 40 },
+            { id: 'q', building: '564 Pacific', title: 'Auditorium Q', occupancy: 40 },
+            { id: 'r', building: '564 Pacific', title: 'Auditorium R', occupancy: 40 },
+            { id: 's', building: '564 Pacific', title: 'Auditorium S', occupancy: 40 },
+            { id: 't', building: '564 Pacific', title: 'Auditorium T', occupancy: 40 },
+            { id: 'u', building: '564 Pacific', title: 'Auditorium U', occupancy: 40 },
+            { id: 'v', building: '564 Pacific', title: 'Auditorium V', occupancy: 40 },
+            { id: 'w', building: '564 Pacific', title: 'Auditorium W', occupancy: 40 },
+            { id: 'x', building: '564 Pacific', title: 'Auditorium X', occupancy: 40 },
+            { id: 'y', building: '564 Pacific', title: 'Auditorium Y', occupancy: 40 },
+            { id: 'z', building: '564 Pacific', title: 'Auditorium Z', occupancy: 40 },
+          ]}
+          events={[
+            { id: '1', resourceId: 'b', start: '2023-01-07T02:00:00', end: '2023-01-07T07:00:00', title: 'event 1' },
+            { id: '2', resourceId: 'c', start: '2023-01-07T05:00:00', end: '2023-01-07T22:00:00', title: 'event 2' },
+            { id: '3', resourceId: 'd', start: '2023-01-06', end: '2023-01-08', title: 'event 3' },
+            { id: '4', resourceId: 'e', start: '2023-01-07T03:00:00', end: '2023-01-07T08:00:00', title: 'event 4' },
+            { id: '5', resourceId: 'f', start: '2023-01-07T00:30:00', end: '2023-01-07T02:30:00', title: 'event 5' },
+            { id: '5', resourceId: 'f', start: '2023-01-07T00:30:00', end: '2023-01-07T02:30:00', title: 'event 5' },
+          ]}
+        />
+      )}
     </div>
   )
 }
