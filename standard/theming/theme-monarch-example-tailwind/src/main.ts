e6737ca58035def60e67e4d06904cbcb5330b5df
Adam Shaw <arshaw@users.noreply.github.com>
2025-06-28 12:37:01 -0400

    rearrange tailwind files

--- standard/theming/theme-monarch-example-tailwind/src/main.ts
+++ standard/theming/theme-monarch-example-tailwind/src/main.ts
@@ -13,7 +13,7 @@ import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
 import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
 import scrollGridPlugin from '@fullcalendar/scrollgrid'
 import timelinePlugin from '@fullcalendar/timeline'
-import themePlugin from '@fullcalendar/theme-monarch'
+import themePlugin from '@fullcalendar/theme-monarch/tailwind'
 
 const enablePremium = false
 const enableDark = false
