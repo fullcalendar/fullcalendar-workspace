fd7851ded334642b4d99b03469ad3358b4b5b599
Adam Shaw <arshaw@users.noreply.github.com>
2025-07-20 15:34:05 -0400

    shadcn + breezy

---
+++ standard/theming/ui-shadcn/src/theme-breezy/event-calendar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { CalendarOptions } from "@fullcalendar/core"
+import { useCalendarController } from "@fullcalendar/react"
+import { EventCalendarToolbar } from '../lib/event-calendar-toolbar.js'
+import { EventCalendarView } from './event-calendar-view.js'
+
+export interface EventCalendarProps extends CalendarOptions {
+  availableViews: string[]
+}
+
+export function EventCalendar({ availableViews, ...options }: EventCalendarProps) {
+  const controller = useCalendarController()
+
+  return (
+    <div className='border rounded-xl'>
+      <EventCalendarToolbar
+        controller={controller}
+        availableViews={availableViews}
+      />
+      <EventCalendarView
+        borderless
+        controller={controller}
+        {...options}
+      />
+    </div>
+  )
+}
