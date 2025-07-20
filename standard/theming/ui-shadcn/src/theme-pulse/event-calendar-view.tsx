d6bd4e2f26b428f5b95d72a38dba9caca860941f
Adam Shaw <arshaw@users.noreply.github.com>
2025-07-20 15:35:23 -0400

    shadcn + pulse

---
+++ standard/theming/ui-shadcn/src/theme-pulse/event-calendar-view.tsx
@@ -0,0 +1,32 @@
+import { CalendarOptions } from '@fullcalendar/core'
+import FullCalendar from '@fullcalendar/react'
+import { createEventCalendarOptions, EventCalendarOptionParams } from '@fullcalendar/theme-pulse/options-event-calendar'
+import { createSlots } from '@fullcalendar/theme-pulse/slots'
+import React from 'react'
+// import { eventCalendarIconOptions } from '../lib/event-calendar-icons.js' // TODO
+
+export const optionParams: EventCalendarOptionParams = {
+}
+
+const baseEventCalendarOptions = createEventCalendarOptions(optionParams)
+
+export function EventCalendarView(options: CalendarOptions) {
+  return (
+    <FullCalendar
+      {...baseEventCalendarOptions.optionDefaults}
+      // {...eventCalendarIconOptions}
+
+      {...createSlots({
+        createElement: React.createElement as any, // HACK
+        Fragment: React.Fragment as any, // HACK
+      }, optionParams)}
+
+      {...options}
+
+      views={{
+        ...baseEventCalendarOptions.views,
+        ...options.views,
+      }}
+    />
+  )
+}
