20de7a77365e62873b4eec3d3b66e87397bae857
Adam Shaw <arshaw@users.noreply.github.com>
2025-08-07 22:48:55 -0400

    debug project for issue 7619

---
+++ examples/react18-issue-7619/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.body.appendChild(document.createElement('div'))).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
