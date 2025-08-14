af98777005e98e6cf55a9d0af189bb8d5c8f5701
Adam Shaw <arshaw@users.noreply.github.com>
2025-08-14 18:12:24 -0400

    some debugging

--- theming/demo-react/src/App.tsx
+++ theming/demo-react/src/App.tsx
@@ -422,6 +422,12 @@ export default function App() {
             {(ui === 'mui') && (
               <MuiCssBaseline />
             )}
+            <SchedulerDemo
+              initialView='resourceTimeGridDay'
+              availableViews={['resourceTimeGridDay', 'resourceTimeGridWeek']}
+              ui={ui}
+              theme={theme}
+            />
             <EventCalendarDemo
               initialView='dayGridMonth'
               ui={ui}
@@ -590,6 +596,7 @@ function EventCalendarDemo(props: DemoProps) {
 
 function SchedulerDemo(props: DemoProps) {
   const SchedulerComponent = schedulerComponentMap[props.ui][props.theme]
+  const vResourceView = props.availableViews?.includes('resourceTimeGridDay')
 
   return (
     <SchedulerComponent
@@ -632,40 +639,47 @@ function SchedulerDemo(props: DemoProps) {
         { id: 'a', building: '460 Bryant', title: 'Auditorium A', occupancy: 40 },
         { id: 'b', building: '460 Bryant', title: 'Auditorium B', occupancy: 40, eventColor: 'green' },
         { id: 'c', building: '460 Bryant', title: 'Auditorium C', occupancy: 40, eventColor: 'orange' },
-        {
-          id: 'd',
-          building: '460 Bryant',
-          title: 'Auditorium D',
-          occupancy: 40,
-          children: [
-            { id: 'd1', title: 'Room D1', occupancy: 10 },
-            { id: 'd2', title: 'Room D2', occupancy: 10 },
-          ],
-        },
-        { id: 'e', building: '460 Bryant', title: 'Auditorium E', occupancy: 40 },
-        { id: 'f', building: '460 Bryant', title: 'Auditorium F', occupancy: 40, eventColor: 'red' },
-        { id: 'g', building: '564 Pacific', title: 'Auditorium G', occupancy: 40 },
-        { id: 'h', building: '564 Pacific', title: 'Auditorium H', occupancy: 40 },
-        { id: 'i', building: '564 Pacific', title: 'Auditorium I', occupancy: 40 },
-        { id: 'j', building: '564 Pacific', title: 'Auditorium J', occupancy: 40 },
-        { id: 'k', building: '564 Pacific', title: 'Auditorium K', occupancy: 40 },
-        { id: 'l', building: '564 Pacific', title: 'Auditorium L', occupancy: 40 },
-        { id: 'm', building: '564 Pacific', title: 'Auditorium M', occupancy: 40 },
-        { id: 'n', building: '564 Pacific', title: 'Auditorium N', occupancy: 40 },
-        { id: 'o', building: '564 Pacific', title: 'Auditorium O', occupancy: 40 },
-        { id: 'p', building: '564 Pacific', title: 'Auditorium P', occupancy: 40 },
-        { id: 'q', building: '564 Pacific', title: 'Auditorium Q', occupancy: 40 },
-        { id: 'r', building: '564 Pacific', title: 'Auditorium R', occupancy: 40 },
-        { id: 's', building: '564 Pacific', title: 'Auditorium S', occupancy: 40 },
-        { id: 't', building: '564 Pacific', title: 'Auditorium T', occupancy: 40 },
-        { id: 'u', building: '564 Pacific', title: 'Auditorium U', occupancy: 40 },
-        { id: 'v', building: '564 Pacific', title: 'Auditorium V', occupancy: 40 },
-        { id: 'w', building: '564 Pacific', title: 'Auditorium W', occupancy: 40 },
-        { id: 'x', building: '564 Pacific', title: 'Auditorium X', occupancy: 40 },
-        { id: 'y', building: '564 Pacific', title: 'Auditorium Y', occupancy: 40 },
-        { id: 'z', building: '564 Pacific', title: 'Auditorium Z', occupancy: 40 },
+        ...(
+          vResourceView ? [] : [
+            {
+              id: 'd',
+              building: '460 Bryant',
+              title: 'Auditorium D',
+              occupancy: 40,
+              children: [
+                { id: 'd1', title: 'Room D1', occupancy: 10 },
+                { id: 'd2', title: 'Room D2', occupancy: 10 },
+              ],
+            },
+            { id: 'e', building: '460 Bryant', title: 'Auditorium E', occupancy: 40 },
+            { id: 'f', building: '460 Bryant', title: 'Auditorium F', occupancy: 40, eventColor: 'red' },
+            { id: 'g', building: '564 Pacific', title: 'Auditorium G', occupancy: 40 },
+            { id: 'h', building: '564 Pacific', title: 'Auditorium H', occupancy: 40 },
+            { id: 'i', building: '564 Pacific', title: 'Auditorium I', occupancy: 40 },
+            { id: 'j', building: '564 Pacific', title: 'Auditorium J', occupancy: 40 },
+            { id: 'k', building: '564 Pacific', title: 'Auditorium K', occupancy: 40 },
+            { id: 'l', building: '564 Pacific', title: 'Auditorium L', occupancy: 40 },
+            { id: 'm', building: '564 Pacific', title: 'Auditorium M', occupancy: 40 },
+            { id: 'n', building: '564 Pacific', title: 'Auditorium N', occupancy: 40 },
+            { id: 'o', building: '564 Pacific', title: 'Auditorium O', occupancy: 40 },
+            { id: 'p', building: '564 Pacific', title: 'Auditorium P', occupancy: 40 },
+            { id: 'q', building: '564 Pacific', title: 'Auditorium Q', occupancy: 40 },
+            { id: 'r', building: '564 Pacific', title: 'Auditorium R', occupancy: 40 },
+            { id: 's', building: '564 Pacific', title: 'Auditorium S', occupancy: 40 },
+            { id: 't', building: '564 Pacific', title: 'Auditorium T', occupancy: 40 },
+            { id: 'u', building: '564 Pacific', title: 'Auditorium U', occupancy: 40 },
+            { id: 'v', building: '564 Pacific', title: 'Auditorium V', occupancy: 40 },
+            { id: 'w', building: '564 Pacific', title: 'Auditorium W', occupancy: 40 },
+            { id: 'x', building: '564 Pacific', title: 'Auditorium X', occupancy: 40 },
+            { id: 'y', building: '564 Pacific', title: 'Auditorium Y', occupancy: 40 },
+            { id: 'z', building: '564 Pacific', title: 'Auditorium Z', occupancy: 40 },
+          ]
+        )
       ]}
-      events='https://fullcalendar.io/api/demo-feeds/events.json?single-day&for-resource-timeline'
+      events={
+        vResourceView ? [] :
+          'https://fullcalendar.io/api/demo-feeds/events.json?single-day&for-resource-timeline'
+      }
     />
   )
 }
