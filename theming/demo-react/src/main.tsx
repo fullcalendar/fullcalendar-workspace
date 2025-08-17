5521c18d6b210378e2286d95bb507d74884b97e7
Adam Shaw <arshaw@users.noreply.github.com>
2025-08-17 17:38:15 -0400

    fonts for demos

--- theming/demo-react/src/main.tsx
+++ theming/demo-react/src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import '@fontsource-variable/inter'
 import './index.css'
 
 import App from './App.js'
