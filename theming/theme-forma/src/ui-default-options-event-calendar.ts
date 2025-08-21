abd05d38b1393950a3e9521d56c3b6f9a3f3263d
Adam Shaw <arshaw@users.noreply.github.com>
2025-08-21 18:42:38 -0400

    darkmode colors for forma

--- theming/theme-forma/src/ui-default-options-event-calendar.ts
+++ theming/theme-forma/src/ui-default-options-event-calendar.ts
@@ -2,54 +2,57 @@ import { CalendarOptions, joinClassNames, ViewOptions } from '@fullcalendar/core
 import { createEventCalendarOptions, EventCalendarOptionParams } from './options-event-calendar.js'
 import * as svgs from './ui-default-svgs.js'
 
+/*
+TODO: focus state!?
+TODO: focus-outline should be black on everything
+TODO: muted text? is #424242 in Outlook
+TODO: kill gray-500's
+*/
+
 export const optionParams: EventCalendarOptionParams = { // TODO: rename to defaultUiParams?
   primaryBgColorClass: 'bg-(--fc-forma-primary-color)',
   primaryTextColorClass: 'text-(--fc-forma-primary-text-color)',
   primaryBorderColorClass: 'border-(--fc-forma-primary-color)', // TODO: rename to compactMoreLinkBorderColorClass?
 
-  majorBorderColorClass: 'border-gray-400 dark:border-gray-700', // TODO: turn into variables!
+  majorBorderColorClass: 'border-(--fc-forma-border3)', // TODO: turn into variables!
 
-  solidMoreLinkBgClass: 'bg-gray-300 dark:bg-gray-600', // TODO: pressability!
+  solidMoreLinkBgClass: 'bg-(--fc-forma-bg6)', // TODO: pressability!
   ghostButtonClass: 'hover:bg-gray-500/20 focus-visible:bg-gray-500/30',
-  selectedBgClass: 'bg-gray-500/40',
+  selectedBgClass: 'bg-gray-500/40', // selected "event"
 
   transparentMutedBgClass: 'bg-gray-500/10',
-  mutedBgClass: 'bg-[#FAFAFA]',
+  mutedBgClass: 'bg-(--fc-forma-bg0)',
 
-  borderColorClass: 'border-[#E0E0E0] dark:border-gray-800',
+  borderColorClass: 'border-(--fc-forma-border1)',
   nowIndicatorBorderColorClass: 'border-(--fc-forma-primary-color)', // same as primary
 
   eventColor: 'var(--fc-forma-event-color)',
   backgroundEventColor: 'var(--color-green-500)',
   backgroundEventColorClass: 'brightness-150 opacity-15',
 
-  popoverClass: 'border border-[#E0E0E0] dark:border-gray-800 bg-(--fc-forma-canvas-color) shadow-md',
+  popoverClass: 'border border-(--fc-forma-border1) bg-(--fc-forma-canvas-color) shadow-md',
 
   bgColorClass: 'bg-(--fc-forma-canvas-color)',
   bgColorOutlineClass: 'outline-(--fc-forma-canvas-color)',
 }
 
-// TODO: focus state!?
-// TODO: focus-outline should be black on everything
-// TODO: muted text? is #424242 in Outlook
-
-const unselectedButtonTextColorClass = 'text-[#242424]'
-const unselectedButtonHoverBorderColorClass = 'hover:border-[#E5E5E5]'
-const unselectedButtonHoverBgColorClass = 'hover:bg-[#FAFAFA]'
-const unselectedButtonActiveBorderColorClass = 'active:border-[#D1D1D1]'
-const unselectedButtonActiveBgColorClass = 'active:bg-[#E5E5E5] focus-visible:bg-[#E5E5E5]'
+const unselectedButtonTextColorClass = 'text-(--fc-forma-muted-text-color)'
+const unselectedButtonHoverBorderColorClass = 'hover:border-(--fc-forma-border0)'
+const unselectedButtonHoverBgColorClass = 'hover:bg-(--fc-forma-bg0)'
+const unselectedButtonActiveBorderColorClass = 'active:border-(--fc-forma-border1)'
+const unselectedButtonActiveBgColorClass = 'active:bg-(--fc-forma-bg4) focus-visible:bg-(--fc-forma-bg4)'
 const unselectedButtonClass = `border-transparent ${unselectedButtonTextColorClass} ${unselectedButtonHoverBorderColorClass} ${unselectedButtonHoverBgColorClass} ${unselectedButtonActiveBorderColorClass} ${unselectedButtonActiveBgColorClass}`
 
-const selectedButtonBorderColorClass = 'border-[#A6A6A6]'
-const selectedButtonBgColorClass = 'bg-[#F5F5F5]'
-const selectedButtonHoverBgColorClass = 'hover:bg-[#F0F0F0]'
-const selectedButtonActiveBgColorClass = 'active:bg-[#EBEBEB] focus-visible:bg-[#EBEBEB]'
+const selectedButtonBorderColorClass = 'border-(--fc-forma-border3)'
+const selectedButtonBgColorClass = 'bg-(--fc-forma-bg1)'
+const selectedButtonHoverBgColorClass = 'hover:bg-(--fc-forma-bg2)'
+const selectedButtonActiveBgColorClass = 'active:bg-(--fc-forma-bg3) focus-visible:bg-(--fc-forma-bg3)'
 const selectedButtonClass = `${selectedButtonBorderColorClass} ${selectedButtonBgColorClass} ${selectedButtonHoverBgColorClass} ${selectedButtonActiveBgColorClass}`
 
 // can be added a-la-carte to other buttons
-const buttonHoverBgColorClass = 'hover:bg-[#EBEBEB] active:bg-[#E0E0E0]'
-const buttonBorderColorClass = 'border-[#E0E0E0] hover:border-[#D1D1D1] active:border-[#D1D1D1] focus-visible:border-[#D1D1D1]'
-const buttonIconColorClass = 'text-[#424242]'
+const buttonHoverBgColorClass = 'hover:bg-(--fc-forma-bg3) active:bg-(--fc-forma-bg5)'
+const buttonBorderColorClass = 'border-(--fc-forma-bg5) hover:border-(--fc-forma-border1) active:border-(--fc-forma-border1) focus-visible:border-(--fc-forma-border1)'
+const buttonIconColorClass = 'text-(--fc-forma-icon-color)'
 
 const buttonIconClass = `text-[1.5em] w-[1em] h-[1em] ${buttonIconColorClass}`
 
