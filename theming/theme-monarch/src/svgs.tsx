ffc1ff520a8ec2eff1cda531240ba259d712075c
Adam Shaw <arshaw@users.noreply.github.com>
2025-10-03 20:32:45 -0400

    forgot svg files

---
+++ theming/theme-monarch/src/svgs.tsx
@@ -0,0 +1,14 @@
+import { createElement, ComponentChild } from '@fullcalendar/core/preact'
+
+export function filledRightTriangle(className?: string): ComponentChild {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 800 2200"
+      preserveAspectRatio="none"
+      className={className}
+     >
+      <polygon points="0,0 66,0 800,1100 66,2200 0,2200" fill="currentColor" />
+    </svg>
+  )
+}
