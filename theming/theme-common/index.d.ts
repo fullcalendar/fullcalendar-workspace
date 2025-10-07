f2763779bdaa9c0ad3a2396268e85878328ee116
Adam Shaw <arshaw@users.noreply.github.com>
2025-10-07 11:07:57 -0400

    theme-common package

---
+++ theming/theme-common/index.d.ts
@@ -0,0 +1,34 @@
+
+/*
+from Pulse
+*/
+export interface ThemeOptionParams {
+  tertiaryClass: string
+  tertiaryPressableClass: string
+
+  ghostHoverClass: string
+  ghostPressableClass: string
+
+  strongBgClass: string
+  mutedBgClass: string
+  mutedWashClass: string
+  highlightClass: string
+
+  borderColorClass: string
+  strongBorderColorClass: string
+  nowBorderColorClass: string
+
+  eventColor: string
+  eventContrastColor: string
+  bgEventColor: string
+  bgEventColorClass: string
+
+  popoverClass: string
+
+  bgClass: string
+  bgOutlineColorClass: string
+
+  fgClass: string
+  strongFgClass: string
+  mutedFgClass: string
+}
