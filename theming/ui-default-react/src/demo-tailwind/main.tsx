4f19add18e325421618daf1bbf5676a83c361039
Adam Shaw <arshaw@users.noreply.github.com>
2025-11-26 18:43:23 -0500

    ui-default-react tailwind demo

---
+++ theming/ui-default-react/src/demo-tailwind/main.tsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.js'
+import './styles.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
