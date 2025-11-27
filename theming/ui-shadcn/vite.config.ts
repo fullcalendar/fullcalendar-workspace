64414292a22ef31b8a952e21f962b9fb5b257684
Adam Shaw <arshaw@users.noreply.github.com>
2025-11-26 19:00:33 -0500

    shadcn demo app

---
+++ theming/ui-shadcn/vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  root: '.',
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/demo'),
+    },
+  },
+})
+
