7fe543c372a25dc06f7c358f8ca8abe695c79188
Adam Shaw <arshaw@users.noreply.github.com>
2025-11-30 16:18:37 -0500

    fork out shoelace theme

--- theming/demo-react/src/pages/ui-default-tailwind-dev.tsx
+++ theming/demo-react/src/pages/ui-default-tailwind-dev.tsx
@@ -4,7 +4,8 @@ import '@fontsource-variable/inter' // for ui-default theme-breezy
 import './lib/root.css'
 import './lib/tailwind.css'
 import './lib/ui-default.css'
-import '@shoelace-style/shoelace/dist/themes/light.css'
+import './lib/light.css'
+import './lib/dark.css'
 import SlRadioButton from '@shoelace-style/shoelace/dist/react/radio-button/index.js'
 import SlRadioGroup from '@shoelace-style/shoelace/dist/react/radio-group/index.js'
 import SlButton from '@shoelace-style/shoelace/dist/react/button/index.js';
