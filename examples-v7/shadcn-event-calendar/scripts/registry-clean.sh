4d54b22b759d94963e2f8640c144271f417d3b21
Adam Shaw <arshaw@users.noreply.github.com>
2025-12-12 17:40:53 -0500

    better registry clean

--- examples-v7/shadcn-event-calendar/scripts/registry-clean.sh
+++ examples-v7/shadcn-event-calendar/scripts/registry-clean.sh
@@ -7,9 +7,7 @@ rm -rf src/components/ui
 git checkout -- src/App.tsx
 git checkout -- package.json
 
-echo
-echo "SUCCESS in cleaning files"
-echo "BUT, you'll still want to revert the PNPM lockfile and reinstall:"
-echo
-echo "  git checkout -- ../pnpm-lock.yaml && pnpm install"
-echo
+# go to monorepo root
+cd ../..
+git checkout -- pnpm-lock.yaml
+pnpm install
