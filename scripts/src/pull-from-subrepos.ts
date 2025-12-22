d0611c561da5428464dfae5bbcefb028a19e7e8f
Adam Shaw <arshaw@users.noreply.github.com>
2025-12-22 18:34:21 -0500

    pull-from-subrepos script

--- scripts/src/pull-from-subrepos.ts
+++ scripts/src/pull-from-subrepos.ts
@@ -1,16 +1,10 @@
 import { join as joinPaths } from 'path'
 import { execLive } from '@fullcalendar-scripts/standard/utils/exec'
 
-/*
-Should be renamed to "push-to-subrepos"
-*/
 export default async function() {
   const monorepoDir = process.cwd()
   const gitSubrepoBin = joinPaths(monorepoDir, 'scripts/bin/git-subrepo.sh') // TODO: DRY
 
   // Push to subrepos. Will fail if our copies are not up-to-date
-  await execLive([gitSubrepoBin, 'push', '--all'])
-
-  // Push the git-subrepo meta-file updates back to our remote
-  await execLive(['git', 'push'])
+  await execLive([gitSubrepoBin, 'pull', '--all'])
 }
