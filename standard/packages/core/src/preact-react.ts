37423754067dba3ef979f6a66cd7b961202418d8
Adam Shaw <arshaw@users.noreply.github.com>
2026-01-09 13:36:14 -0500

    react rig

---
+++ standard/packages/core/src/preact-react.ts
@@ -0,0 +1,34 @@
+
+export function flushUpdates(): void {
+  // not possible
+}
+
+export const preactOptions: any = {}
+
+export {
+  type JSX,
+  type ReactElement as VNode,
+  type ComponentType,
+  type ReactNode as ComponentChild,
+  type ReactNode as ComponentChildren,
+  type Context,
+  type Ref,
+  type RefObject,
+  Component,
+  createElement,
+  createContext,
+  createRef,
+  Fragment,
+  isValidElement,
+  FC as FunctionalComponent,
+} from 'react'
+
+export {
+  createPortal,
+  flushSync,
+} from 'react-dom'
+
+export {
+  createRoot,
+} from 'react-dom/client'
+
