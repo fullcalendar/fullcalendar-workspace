1df5371249860d3a2b7a0acdf44cedeae0494e76
Adam Shaw <arshaw@users.noreply.github.com>
2026-01-14 22:48:03 -0500

    standard theme

---
+++ standard/packages/vanilla/src/preact.ts
@@ -0,0 +1,7 @@
+
+// temporary while we use react
+
+export { createElement, Fragment  } from 'react'
+export {  } from 'react/jsx-runtime'
+
+export {} from 'preact/jsx-dev-runtime'
---
+++ standard/packages/vanilla/src/themes/classic/theme.ts
@@ -0,0 +1,1 @@
+import '@fullcalendar/preact/themes/classic/theme.css'
