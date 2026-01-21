47a95231e60cd7ca632d1fcd604027160c420b00
Adam Shaw <arshaw@users.noreply.github.com>
2026-01-21 18:10:08 -0500

    get react-scheduler building

---
+++ premium/packages/react-scheduler/scripts/src-generate.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# Get the directory where this script lives
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pkg_dir="$(dirname "$script_dir")"
+
+# Remove old src and copy fresh from preact-scheduler
+rm -rf "$pkg_dir/src"
+cp -r "$pkg_dir/../preact-scheduler/src" "$pkg_dir/src"
+
+# Replace @fullcalendar/preact with @fullcalendar/react2 in all files
+find "$pkg_dir/src" -type f -exec sed -i '' 's/@fullcalendar\/preact/@fullcalendar\/react2/g' {} +
