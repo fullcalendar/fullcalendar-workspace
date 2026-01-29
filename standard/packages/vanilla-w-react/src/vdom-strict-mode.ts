dd6e0d3afac49054c580d2b15101727e9aa3e6cb
Adam Shaw <arshaw@users.noreply.github.com>
2026-01-29 16:41:48 -0500

    finalize strict mode stuff

--- standard/packages/vanilla-w-react/src/vdom-strict-mode.ts
+++ standard/packages/vanilla-w-react/src/vdom-strict-mode.ts
@@ -1,5 +1,5 @@
 import { Fragment as ReactFragment, StrictMode as ReactStrictMode } from 'react'
 
 // Manually change this const to 2 to enable StrictMode
-export const strictModeFactor = 2
+export const strictModeFactor = 1
 export const StrictMode = strictModeFactor > 1 ? ReactStrictMode : ReactFragment
