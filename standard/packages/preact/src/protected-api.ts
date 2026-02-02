0bab62b6420a4e40b155abeadde5a712787a6ad0
Adam Shaw <arshaw@users.noreply.github.com>
2026-02-02 14:37:50 -0500

    remove unused internal api exports

--- standard/packages/preact/src/protected-api.ts
+++ standard/packages/preact/src/protected-api.ts
@@ -1,9 +1,7 @@
 
 export {
-  BASE_OPTION_DEFAULTS,
   CalendarListeners,
   CalendarListenersRefined,
-  ViewOptions,
   ViewOptionsRefined,
   Identity,
   Dictionary,
@@ -12,10 +10,8 @@ export {
 } from './options'
 
 export type { EventDef, EventDefHash } from './structs/event-def'
-export type { EventInstance, EventInstanceHash } from './structs/event-instance'
-export { createEventInstance } from './structs/event-instance'
-export type { EventRefined, EventTuple } from './structs/event-parse'
-export { parseEventDef, refineEventDef } from './structs/event-parse'
+export type { EventInstanceHash } from './structs/event-instance'
+export type { EventRefined } from './structs/event-parse'
 export { parseBusinessHours } from './structs/business-hours'
 
 export type { OrderSpec } from './util/misc'
@@ -23,23 +19,12 @@ export {
   parseFieldSpecs,
   compareByFieldSpecs,
   flexibleCompare,
-  preventSelection, allowSelection, preventContextMenu, allowContextMenu,
-  compareNumbers, enableCursor, disableCursor,
   guid,
 } from './util/misc'
 
 export {
   computeVisibleDayRange,
-  diffDates,
 } from './util/date'
-
-export {
-  WEEKDAY_ONLY_FORMAT,
-  FULL_DATE_FORMAT,
-  findWeekdayText,
-  findDayNumberText,
-  findMonthText,
-} from './util/date-format'
 
 export {
   removeExact,
@@ -48,17 +33,12 @@ export {
 
 export { memoize, memoizeObjArg } from './util/memoize'
 
-export type { Rect, Point } from './util/geom'
 export {
-  intersectRects,
-  pointInsideRect,
-  constrainPoint,
-  getRectCenter, diffPoints,
-  translateRect,
+  getRectCenter,
 } from './util/geom'
 
 export {
-  mapHash, filterHash, isPropsEqualShallow, isPropsEqualWithMap,
+  mapHash, filterHash, isPropsEqualShallow,
 } from './util/object'
 
 export {
@@ -70,51 +50,34 @@ export {
 } from './options-manip'
 
 export {
-  getAppendableRoot,
   computeElIsRtl,
-  applyStyle,
   applyStyleProp,
-  getEventTargetViaRoot,
 } from './util/dom-manip'
-export { joinArrayishClassNames, fracToCssDim } from './util/html'
+export { joinArrayishClassNames } from './util/html'
 
 export type { EventStore } from './structs/event-store'
 export {
-  createEmptyEventStore,
   mergeEventStores,
-  getRelevantEvents,
-  eventTupleToStore,
 } from './structs/event-store'
 export type { EventUiHash, EventUi } from './component-util/event-ui'
 export { combineEventUis, createEventUi } from './component-util/event-ui'
 export type { SplittableProps } from './component-util/event-splitting'
 export { Splitter } from './component-util/event-splitting'
 export { getDateMeta, DateMeta } from './component-util/date-rendering'
-export { isDimsEqual } from './component-util/rendering-misc'
 export { watchSize, watchWidth, watchHeight, afterSize } from './component-util/resize-observer'
 export { debounce } from './util/debounce'
 
-export { buildNavLinkAttrs, buildDateStr } from './common/nav-link'
-
-export {
-  preventDefault,
-  whenTransitionDone,
-} from './util/dom-event'
+export { buildNavLinkAttrs } from './common/nav-link'
 
 export {
   computeInnerRect,
   computeEdges,
-  getClippingParents,
-  computeRect,
 } from './util/dom-geom'
 
 export { unpromisify } from './util/promise'
 
 export { Emitter } from './common/Emitter'
-export { PositionCache } from './common/PositionCache'
-export { ScrollController, ElementScrollController, WindowScrollController } from './common/scroll-controller'
 export type { ViewContext } from './ViewContext'
-export { ViewContextType } from './ViewContext'
 export type { EventSegUiInteractionState } from './component/DateComponent'
 export { DateComponent } from './component/DateComponent'
 export type { CalendarData } from './reducers/data-types'
@@ -123,56 +86,30 @@ export type { ViewProps } from './component-util/View'
 
 export type { DateProfile } from './DateProfileGenerator'
 export { DateProfileGenerator, computeMajorUnit, isMajorUnit } from './DateProfileGenerator'
-export type { ViewSpec } from './structs/view-spec'
 export type { DateSpan } from './structs/date-span'
-export { isDateSpansEqual } from './structs/date-span'
-
-export type { EventSourceDef } from './structs/event-source-def'
-export type { EventSourceRefined } from './structs/event-source-parse'
 
 export {
-  EventPlacement,
-  EventInsertion,
   SegHierarchy,
   SegGroup,
   groupIntersectingSegs,
-  binarySearch,
 } from './seg-hierarchy'
 
-export type { InteractionSettings, InteractionSettingsStore } from './interactions/interaction'
-export {
-  Interaction,
-  interactionSettingsToStore,
-  interactionSettingsStore,
-} from './interactions/interaction'
 export type { PointerDragEvent } from './interactions/pointer'
 export type { Hit } from './interactions/hit'
-export type { dateSelectionJoinTransformer } from './interactions/date-selecting'
-export type { eventDragMutationMassager, EventDropTransformers } from './interactions/event-dragging'
 export { ElementDragging } from './interactions/ElementDragging'
 
 export { config } from './global-config'
 
-export type { RecurringType } from './structs/recurring-event'
-
-export type { DragMetaInput, DragMeta } from './structs/drag-meta'
-export { parseDragMeta } from './structs/drag-meta'
-
-export type { ViewPropsTransformer, PluginDef } from './plugin-system-struct'
+export type { ViewPropsTransformer } from './plugin-system-struct'
 export type { Action } from './reducers/Action'
 export type { CalendarContext } from './CalendarContext'
 export type { CalendarContentProps } from './CalendarInner'
 export { CalendarInner } from './CalendarInner'
 
-export { DaySeriesModel } from './common/DaySeriesModel'
-
 export type { EventInteractionState } from './interactions/event-interaction-state'
 export {
-  sliceEventStore, hasBgRendering, getElEventRange,
-  buildEventRangeTimeText,
   sortEventSegs,
   getEventRangeMeta, buildEventRangeKey,
-  getEventTagAndAttrs,
   EventRangeProps,
   getEventKey,
   MinimalEventProps,
@@ -181,19 +118,18 @@ export {
 export type { DayTableCell, DayGridRange } from './common/DayTableModel'
 export { DayTableModel } from './common/DayTableModel'
 
-export { Scroller, getNormalizedScrollX, setNormalizedScrollX } from './scrollgrid/Scroller'
+export { Scroller } from './scrollgrid/Scroller'
 
 export type { SlicedProps } from './common/slicing-utils'
 export { Slicer } from './common/slicing-utils'
 
 export type { EventMutation } from './structs/event-mutation'
-export { applyMutationToEventStore } from './structs/event-mutation'
 export type { Constraint } from './structs/constraint'
-export { isPropsValid, isInteractionValid, isDateSelectionValid } from './validation'
+export { isPropsValid } from './validation'
 
 export { requestJson } from './util/requestJson'
 
-export { flushUpdates, flushSync } from './vdom'
+export { flushSync } from './vdom'
 export { BaseComponent, setRef } from './vdom-util'
 export { DelayedRunner } from './util/DelayedRunner'
 
@@ -201,13 +137,11 @@ export {
   getStickyFooterScrollbar,
   getStickyHeaderDates,
   getIsHeightAuto,
-  getScrollerSyncerClass,
 } from './scrollgrid/util'
 
 // new
-export { ScrollerInterface } from './scrollgrid/ScrollerInterface'
 export { ScrollerSyncerInterface } from './scrollgrid/ScrollerSyncerInterface'
-export { Ruler, RulerProps } from './scrollgrid/Ruler'
+export { Ruler } from './scrollgrid/Ruler'
 
 export { RefMap } from './util/RefMap'
 
@@ -215,7 +149,7 @@ export { NowTimer } from './NowTimer'
 export type {
   CustomContentGenerator, DidMountHandler, WillUnmountHandler, MountData,
 } from './common/render-hook'
-export { StandardEvent, StandardEventProps } from './common/StandardEvent'
+export { StandardEvent } from './common/StandardEvent'
 export { NowIndicatorHeaderContainer } from './common/NowIndicatorHeaderContainer'
 export { NowIndicatorLineContainer } from './common/NowIndicatorLineContainer'
 export { NowIndicatorDot } from './common/NowIndicatorDot'
@@ -223,21 +157,15 @@ export { NowIndicatorDot } from './common/NowIndicatorDot'
 export { renderFill, BgEvent } from './common/bg-fill'
 export { MoreLinkContainer } from './common/MoreLinkContainer'
 
-export type { ViewContainerProps } from './common/ViewContainer'
 export { ViewContainer } from './common/ViewContainer'
-export type { DatePointTransform, DateSpanTransform } from './calendar-utils'
-export { triggerDateSelect, getDefaultEventEnd } from './calendar-utils'
 
 export { CalendarApiImpl } from './api/CalendarApiImpl'
-export { EventImpl, buildEventApis } from './api/EventImpl'
+export { EventImpl } from './api/EventImpl'
 
 export { CalendarMediaRoot, computeRootClassName } from './calendar-root'
 export { RenderId } from './content-inject/RenderId'
 
-export type { ElProps } from './content-inject/ContentInjector'
-export { buildElAttrs } from './content-inject/ContentInjector'
-export type { InnerContainerFunc } from './content-inject/ContentContainer'
-export { ContentContainer, renderText, generateClassName } from './content-inject/ContentContainer'
+export { ContentContainer, generateClassName } from './content-inject/ContentContainer'
 export type { CustomRendering } from './content-inject/CustomRenderingStore'
 export { CustomRenderingStore } from './content-inject/CustomRenderingStore'
 
@@ -245,60 +173,41 @@ export {
   CoordRange,
   CoordSpan,
   SlicedCoordRange,
-  computeEarliestStart,
-  computeLatestEnd,
-  getCoordRangeEnd,
 } from './coord-range'
 
 export { FooterScrollbar } from './common/FooterScrollbar'
 
 export { createFormatter } from './datelib/formatting'
 
-export type { DateRange, DateMarker, DateFormatter, VerboseFormattingData } from '@full-ui/headless-calendar'
+export type { DateRange, DateMarker, DateFormatter } from '@full-ui/headless-calendar'
 export {
-  rangeContainsMarker, intersectRanges, rangesEqual, rangesIntersect, rangeContainsRange,
-  addDays, startOfDay, addMs, addWeeks, diffWeeks, diffWholeWeeks, diffWholeDays, diffDayAndTime, diffDays, isValidDate,
-  createDuration, asCleanDays, multiplyDuration, addDurations, asRoughMinutes, asRoughSeconds, asRoughMs, wholeDivideDurations, greatestDurationDenominator,
+  rangeContainsMarker, intersectRanges, rangesEqual, rangesIntersect,
+  addDays, startOfDay, addMs, diffWholeWeeks, diffWholeDays, diffDayAndTime, isValidDate,
+  createDuration, asCleanDays, multiplyDuration, asRoughMinutes, asRoughSeconds, asRoughMs, wholeDivideDurations, greatestDurationDenominator,
   DateEnv,
-  formatIsoTimeString, formatDayString, buildIsoString, formatIsoMonthStr,
-  NamedTimeZoneImpl,
+  formatDayString,
   parseMarker,
-  padStart, isInt, trimEnd,
+  padStart, isInt,
 } from '@full-ui/headless-calendar'
 
 export { TimeGridRange, organizeSegsByCol, splitInteractionByCol } from './timegrid/TimeColsSeg'
 export { DayTimeColsSlicer } from './timegrid/DayTimeColsSlicer'
-export { TimeSlatMeta, buildSlatMetas } from './timegrid/time-slat-meta'
 export { AllDaySplitter } from './timegrid/AllDaySplitter'
-export { TimeGridView } from './timegrid/components/TimeGridView'
-export { TimeGridLayout, TimeGridLayoutProps } from './timegrid/components/TimeGridLayout'
-export { TimeGridWeekNumber } from './timegrid/components/TimeGridWeekNumber'
+export { TimeGridLayout } from './timegrid/components/TimeGridLayout'
 export { buildTimeColsModel, buildDayRanges } from './timegrid/components/util'
 
 export { DayTableSlicer } from './daygrid/DayTableSlicer'
-export { TableDateProfileGenerator, buildDayTableRenderRange } from './daygrid/TableDateProfileGenerator'
-export { DayGridView } from './daygrid/components/DayGridView'
-export { DayGridHeaderRow, DayGridHeaderRowProps } from './daygrid/components/DayGridHeaderRow'
 export {
   CellRenderConfig,
   CellDataConfig,
   RowConfig,
-  buildDateRowConfigs,
   buildDateRowConfig,
   buildDateRenderConfig,
   buildDateDataConfigs,
 } from './daygrid/header-tier'
-export { createDayHeaderFormatter, dayHeaderMicroFormat, dayMicroWidth } from './daygrid/components/util'
-export { DayGridLayout, DayGridLayoutProps } from './daygrid/components/DayGridLayout'
-export { computeRowBasis } from './daygrid/components/DayGridRows'
-export { DayGridRow, DayGridRowProps } from './daygrid/components/DayGridRow'
-export { DayGridRows } from './daygrid/components/DayGridRows'
+export { createDayHeaderFormatter } from './daygrid/components/util'
+export { DayGridLayout } from './daygrid/components/DayGridLayout'
 export {
   buildDayTableModel,
-  computeColWidth,
-  computeColFromPosition,
-  getRowEl,
-  getCellEl,
 } from './daygrid/components/util'
 
-export { ListView } from './list/components/ListView'
