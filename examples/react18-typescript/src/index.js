a2a2f236c5ddecd086f49073eac29967e91152b4
Adam Shaw <arshaw@users.noreply.github.com>
2026-04-07 20:30:00 -0400

    fix issues with react18-typescript project

--- examples/react18-typescript/src/index.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { createRoot } from 'react-dom/client';
-import DemoApp from './DemoApp';
-import './index.css';
-createRoot(document.getElementById('root')).render(_jsx(DemoApp, {}));
-//# sourceMappingURL=index.js.map
