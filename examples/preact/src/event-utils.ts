594bd23472927dc38942c4664aff4078796593a2
Adam Shaw <arshaw@users.noreply.github.com>
2026-04-24 14:43:14 -0400

    preact example projects

---
+++ examples/preact/src/event-utils.ts
@@ -0,0 +1,21 @@
+import { EventInput } from '@fullcalendar/preact'
+
+let eventGuid = 0
+const todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS: EventInput[] = [
+  {
+    id: createEventId(),
+    title: 'All-day event',
+    start: todayStr
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: todayStr + 'T12:00:00'
+  }
+]
+
+export function createEventId() {
+  return String(eventGuid++)
+}
