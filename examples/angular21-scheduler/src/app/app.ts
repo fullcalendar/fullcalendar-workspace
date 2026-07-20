121c0605ed61adbccc78e84e8aec11c62c390584
Adam Shaw <arshaw@users.noreply.github.com>
2026-07-20 14:01:51 -0400

    temporary minimal repro commit

--- examples/angular21-scheduler/src/app/app.ts
+++ examples/angular21-scheduler/src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FullCalendarModule, CalendarOptions, DateSelectInfo, EventClickInfo, EventApi } from '@fullcalendar/angular';
+import {
+  FullCalendarModule,
+  CalendarOptions,
+  EventApi,
+} from '@fullcalendar/angular';
 import themePlugin from '@fullcalendar/angular/themes/classic';
-import interactionPlugin from '@fullcalendar/angular/interaction';
-import dayGridPlugin from '@fullcalendar/angular/daygrid';
-import timeGridPlugin from '@fullcalendar/angular/timegrid';
-import listPlugin from '@fullcalendar/angular/list';
-import resourceTimeGridPlugin from '@fullcalendar/angular-scheduler/resource-timegrid'
 import resourceTimelinePlugin from '@fullcalendar/angular-scheduler/resource-timeline'
-import { RESOURCES, INITIAL_EVENTS, createEventId } from './event-utils';
+import { RESOURCES, INITIAL_EVENTS } from './event-utils';
 
 @Component({
   selector: 'app-root',
@@ -20,36 +19,18 @@ import { RESOURCES, INITIAL_EVENTS, createEventId } from './event-utils';
 export class App {
   calendarVisible = signal(true);
   calendarOptions = signal<CalendarOptions>({
-    plugins: [
-      themePlugin,
-      interactionPlugin,
-      dayGridPlugin,
-      timeGridPlugin,
-      listPlugin,
-      resourceTimeGridPlugin,
-      resourceTimelinePlugin,
-    ],
+    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    plugins: [resourceTimelinePlugin, themePlugin],
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'resourceTimelineDay,resourceTimeGridDay,dayGridMonth'
+      right: '',
     },
     initialView: 'resourceTimelineDay',
-    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+    initialDate: '2024-06-10',
     resources: RESOURCES,
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true,
-    select: this.handleDateSelect.bind(this),
-    eventClick: this.handleEventClick.bind(this),
-    eventsSet: this.handleEvents.bind(this)
-    /* you can update a remote database when these fire:
-    eventAdd:
-    eventChange:
-    eventRemove:
-    */
+    events: INITIAL_EVENTS,
+    eventsSet: this.handleEvents.bind(this),
   });
   currentEvents = signal<EventApi[]>([]);
 
@@ -62,30 +43,6 @@ export class App {
       ...options,
       weekends: !options.weekends,
     }));
-  }
-
-  handleDateSelect(selectInfo: DateSelectInfo) {
-    const title = prompt('Please enter a new title for your event');
-    const calendarApi = selectInfo.view.calendar;
-
-    calendarApi.unselect(); // clear date selection
-
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        resourceId: selectInfo.resource?.id,
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      });
-    }
-  }
-
-  handleEventClick(clickInfo: EventClickInfo) {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove();
-    }
   }
 
   handleEvents(events: EventApi[]) {
--- examples/angular21-scheduler/src/app/event-utils.ts
+++ examples/angular21-scheduler/src/app/event-utils.ts
@@ -1,37 +1,15 @@
 import { EventInput } from '@fullcalendar/angular';
 
-let eventGuid = 0;
-const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
-
 export const INITIAL_EVENTS: EventInput[] = [
   {
-    id: createEventId(),
-    resourceId: 'a',
-    title: 'All-day event',
-    start: TODAY_STR
+    id: 'before',
+    resourceId: 'r1',
+    title: 'Starts before visible range',
+    start: '2024-06-09T20:00:00',
+    end: '2024-06-10T05:00:00',
   },
-  {
-    id: createEventId(),
-    resourceId: 'a',
-    title: 'Timed event',
-    start: TODAY_STR + 'T00:00:00',
-    end: TODAY_STR + 'T03:00:00'
-  },
-  {
-    id: createEventId(),
-    resourceId: 'b',
-    title: 'Timed event',
-    start: TODAY_STR + 'T12:00:00',
-    end: TODAY_STR + 'T15:00:00'
-  }
 ];
 
-export function createEventId() {
-  return String(eventGuid++);
-}
-
 export const RESOURCES = [
-  { id: 'a', title: 'Auditorium A' },
-  { id: 'b', title: 'Auditorium B', eventColor: 'green' },
-  { id: 'c', title: 'Auditorium C', eventColor: 'orange' },
-]
+  { id: 'r1', title: 'Resource A' },
+];
