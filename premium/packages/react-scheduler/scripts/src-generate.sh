9d050d6cc75771dea8ce299e03ed1a10f025305a
Adam Shaw <arshaw@users.noreply.github.com>
2026-08-02 11:25:40 -0400

    fix react src-generate script

--- premium/packages/react-scheduler/scripts/src-generate.sh
+++ premium/packages/react-scheduler/scripts/src-generate.sh
@@ -12,5 +12,5 @@ cp -r "$pkg_dir/../preact-scheduler/src" "$pkg_dir/src"
 # Replace @fullcalendar/preact with @fullcalendar/react in all source files
 find "$pkg_dir/src" \
   -type f \( -name '*.ts' -o -name '*.tsx' \) \
-  -exec sed -i.bak 's/@fullcalendar\/preact\//@fullcalendar\/react\//g' {} +
+  -exec sed -i.bak 's/@fullcalendar\/preact/@fullcalendar\/react/g' {} +
 find "$pkg_dir/src" -type f -name '*.bak' -delete
