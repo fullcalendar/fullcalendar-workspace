77b26f211a78045de78b9d1a4cfa9ebeb5c20ac3
Adam Shaw <arshaw@users.noreply.github.com>
2026-08-31 23:40:05 -0400

    Correct resource-timeline row print structure

--- premium/packages/preact-scheduler/src/resource-timeline/components/ResourcePrintRow.tsx
+++ premium/packages/preact-scheduler/src/resource-timeline/components/ResourcePrintRow.tsx
@@ -1,4 +1,4 @@
-import { type ClassNameInput, CssDimValue, joinClassNames } from '@fullcalendar/preact/public-api'
+import { CssDimValue, joinClassNames } from '@fullcalendar/preact/public-api'
 import {
   ContentContainer,
   DateMarker,
@@ -9,7 +9,6 @@ import {
   SplittableProps,
 } from '@fullcalendar/preact/protected-api'
 import classNames from '@fullcalendar/preact/protected-styles'
-import { type ReactNode } from 'react'
 import { TimelineBg } from '../../timeline/components/TimelineBg'
 import {
   TimelinePrintEventBand,
@@ -54,7 +53,7 @@ export interface ResourcePrintRowProps extends SplittableProps, AriaCellInput {
   indentWidth: number | undefined
 }
 
-/** Renders one logical resource row as independently breakable synchronized sections. */
+/** Renders one logical resource row whose Timeline bands can fragment across print pages. */
 export class ResourcePrintRow extends TimelinePrintRenderer<ResourcePrintRowProps> {
   // memo
   private refineRenderProps = memoizeObjArg(refineResourceLaneRenderProps)
@@ -72,8 +71,6 @@ export class ResourcePrintRow extends TimelinePrintRenderer<ResourcePrintRowProp
       context,
       eventOverlap: Boolean(options.eventOverlap),
     })
-    // Generate this render hook once. Its CSS tokens are also reused by the
-    // trailing section solely to draw the lane's real row-bottom border.
     const resourceLaneClassName = generateClassName(options.resourceLaneClass, renderProps)
 
     const slicedProps = this.slicer.sliceProps(
@@ -91,27 +88,6 @@ export class ResourcePrintRow extends TimelinePrintRenderer<ResourcePrintRowProp
       props.tDateProfile,
       props.slotWidth,
     )
-    const leadingColGroupStats = props.colGroupStats.map((stats) => ({
-      ...stats,
-      borderBottom: false,
-    }))
-
-    const sectionFrameProps = {
-      spreadsheetWidth: props.spreadsheetWidth,
-      dividerClassName: options.resourceColumnDividerClass,
-    }
-    const placeholderProps = {
-      layout,
-      colGroupStats: props.colGroupStats,
-      colSpecs: props.colSpecs,
-      groupColCnt: props.groupColCnt,
-      colWidths: props.colWidths,
-      colGrows: props.colGrows,
-      spreadsheetCanvasWidth: props.spreadsheetCanvasWidth,
-      resourceRowClassName: options.resourceRowClass,
-      resourceBorderBottom: false,
-      drawBottomBorders: false,
-    }
     const timelineBandProps = {
       dateProfile: props.dateProfile,
       tDateProfile: props.tDateProfile,
@@ -127,109 +103,85 @@ export class ResourcePrintRow extends TimelinePrintRenderer<ResourcePrintRowProp
         role='row'
         aria-level={props.hasNesting ? layout.indent : undefined}
         aria-expanded={layout.hasChildren ? layout.isExpanded : undefined}
-        className={classNames.rel}
+        className={classNames.flexRow}
       >
         <div
-          aria-hidden
-          className={joinClassNames(classNames.fill, classNames.flexRow, classNames.z0)}
-          style={{ height: '100%' }}
+          className={joinClassNames(classNames.flexCol, classNames.crop)}
+          style={{ width: props.spreadsheetWidth }}
         >
           <div
-            className={classNames.noShrink}
-            style={{ width: props.spreadsheetWidth }}
-          />
-          <div
-            className={joinClassNames(
-              options.resourceColumnDividerClass,
-              classNames.invisible,
-            )}
-          />
-          <div className={joinClassNames(classNames.liquidX, classNames.crop, classNames.rel)}>
-            <div
-              className={classNames.fillY}
-              style={{
-                width: props.timeCanvasWidth,
-                height: '100%',
-                insetInlineStart: -props.timeAreaOffset,
-              }}
-            >
-              <TimelineBg
-                tDateProfile={props.tDateProfile}
-                nowDate={props.nowDate}
-                nowMs={props.nowMs}
-                todayRange={props.todayRange}
-                bgEventSegs={slicedProps.bgEventSegs}
-                businessHourSegs={null}
-                dateSelectionSegs={null}
-                eventResizeSegs={null}
-                slotWidth={props.slotWidth}
-              />
-            </div>
+            className={joinClassNames(classNames.grow, classNames.flexRow)}
+            style={{ minWidth: props.spreadsheetCanvasWidth }}
+          >
+            <ResourceGroupSubrows
+              colGroups={layout.colGroups}
+              colGroupStats={props.colGroupStats}
+              colWidths={props.colWidths}
+              colGrows={props.colGrows}
+            />
+            <ResourceSubrow
+              resource={resource}
+              resourceFields={layout.resourceFields}
+              indent={layout.indent}
+              hasChildren={layout.hasChildren}
+              isExpanded={layout.isExpanded}
+              colStartIndex={props.groupColCnt}
+              colSpecs={props.colSpecs}
+              colWidths={props.colWidths}
+              colGrows={props.colGrows}
+              borderStart={Boolean(props.groupColCnt)}
+              borderBottom={props.isNotLast}
+              indentWidth={props.indentWidth}
+              totalX
+            />
           </div>
         </div>
 
-        <ResourcePrintSection
-          {...sectionFrameProps}
-          key='leading'
-          spreadsheetContent={(
-            <div
-              className={joinClassNames(classNames.grow, classNames.flexRow)}
-              style={{ minWidth: props.spreadsheetCanvasWidth }}
-            >
-              <ResourceGroupSubrows
-                colGroups={layout.colGroups}
-                colGroupStats={leadingColGroupStats}
-                colWidths={props.colWidths}
-                colGrows={props.colGrows}
-              />
-              <ResourceSubrow
-                resource={resource}
-                resourceFields={layout.resourceFields}
-                indent={layout.indent}
-                hasChildren={layout.hasChildren}
-                isExpanded={layout.isExpanded}
-                colStartIndex={props.groupColCnt}
-                colSpecs={props.colSpecs}
-                colWidths={props.colWidths}
-                colGrows={props.colGrows}
-                borderStart={Boolean(props.groupColCnt)}
-                borderBottom={false}
-                indentWidth={props.indentWidth}
-                totalX
-              />
-            </div>
-          )}
-          timelineContent={(
-            <div
-              className={joinClassNames(classNames.flexRow, classNames.rel, classNames.grow)}
-              style={{
-                width: props.timeCanvasWidth,
-                insetInlineStart: -props.timeAreaOffset,
+        <div className={joinClassNames(options.resourceColumnDividerClass)} />
+
+        <div className={joinClassNames(classNames.flexCol, classNames.crop, classNames.liquid)}>
+          <div
+            className={joinClassNames(classNames.flexCol, classNames.rel, classNames.grow)}
+            style={{
+              width: props.timeCanvasWidth,
+              insetInlineStart: -props.timeAreaOffset,
+            }}
+          >
+            <ContentContainer
+              tag='div'
+              attrs={{
+                ...buildAriaCellAttrs(props),
+                role: 'gridcell',
+                'data-resource-id': resource.id,
               }}
+              className={joinClassNames(
+                resourceLaneClassName,
+                classNames.grow,
+                classNames.noMargin,
+                classNames.noPadding,
+                classNames.flexCol,
+                classNames.contentBox,
+                props.isNotLast ? classNames.borderOnlyB : classNames.borderNone,
+                classNames.rel,
+              )}
+              renderProps={renderProps}
+              generatorName={undefined}
+              didMount={options.resourceLaneDidMount}
+              willUnmount={options.resourceLaneWillUnmount}
             >
-              <ContentContainer
-                tag='div'
-                attrs={{
-                  ...buildAriaCellAttrs(props),
-                  role: 'gridcell',
-                  'data-resource-id': resource.id,
-                }}
-                className={joinClassNames(
-                  resourceLaneClassName,
-                  classNames.liquid,
-                  classNames.noMargin,
-                  classNames.noPadding,
-                  classNames.flexCol,
-                  classNames.contentBox,
-                  classNames.borderNone,
-                  classNames.rel,
-                )}
-                renderProps={renderProps}
-                generatorName={undefined}
-                didMount={options.resourceLaneDidMount}
-                willUnmount={options.resourceLaneWillUnmount}
-              >
-                {() => (
+              {() => (
+                <>
+                  <TimelineBg
+                    tDateProfile={props.tDateProfile}
+                    nowDate={props.nowDate}
+                    nowMs={props.nowMs}
+                    todayRange={props.todayRange}
+                    bgEventSegs={slicedProps.bgEventSegs}
+                    businessHourSegs={null}
+                    dateSelectionSegs={null}
+                    eventResizeSegs={null}
+                    slotWidth={props.slotWidth}
+                  />
                   <ContentContainer
                     tag='div'
                     className={joinClassNames(classNames.noMargin, classNames.noShrink)}
@@ -238,246 +190,35 @@ export class ResourcePrintRow extends TimelinePrintRenderer<ResourcePrintRowProp
                     customGenerator={options.resourceLaneTopContent}
                     classNameGenerator={options.resourceLaneTopClass}
                   />
-                )}
-              </ContentContainer>
-            </div>
-          )}
-        />
-
-        {eventBands.map((band) => (
-          <ResourcePrintSection
-            {...sectionFrameProps}
-            key={`event-${band.levelIndex}`}
-            spreadsheetContent={<SpreadsheetPlaceholderCells {...placeholderProps} />}
-            timelineContent={(
-              <div
-                className={joinClassNames(classNames.flexCol, classNames.rel)}
-                style={{
-                  width: props.timeCanvasWidth,
-                  insetInlineStart: -props.timeAreaOffset,
-                }}
-              >
-                <TimelinePrintEventBand
-                  {...timelineBandProps}
-                  band={band}
-                  heightRefMap={this.printHeights.segHeightRefMap}
-                />
-              </div>
-            )}
-          />
-        ))}
-
-        {moreLinkBand && (
-          <ResourcePrintSection
-            {...sectionFrameProps}
-            key='more-link'
-            spreadsheetContent={<SpreadsheetPlaceholderCells {...placeholderProps} />}
-            timelineContent={(
-              <div
-                className={joinClassNames(classNames.flexCol, classNames.rel)}
-                style={{
-                  width: props.timeCanvasWidth,
-                  insetInlineStart: -props.timeAreaOffset,
-                }}
-              >
-                <TimelinePrintMoreLinkBand
-                  {...timelineBandProps}
-                  band={moreLinkBand}
-                  heightRefMap={this.printHeights.moreLinkHeightRefMap}
-                />
-              </div>
-            )}
-          />
-        )}
-
-        <ResourcePrintSection
-          {...sectionFrameProps}
-          key='trailing'
-          spreadsheetContent={(
-            <SpreadsheetPlaceholderCells
-              {...placeholderProps}
-              resourceBorderBottom={props.isNotLast}
-              drawBottomBorders
-            />
-          )}
-          timelineContent={(
-            <div
-              className={joinClassNames(classNames.flexCol, classNames.rel)}
-              style={{
-                width: props.timeCanvasWidth,
-                insetInlineStart: -props.timeAreaOffset,
-              }}
-            >
-              <ContentContainer
-                tag='div'
-                className={joinClassNames(
-                  resourceLaneClassName,
-                  classNames.noMargin,
-                  classNames.noPadding,
-                  classNames.noShrink,
-                  classNames.contentBox,
-                  props.isNotLast ? classNames.borderOnlyB : classNames.borderNone,
-                )}
-                renderProps={renderProps}
-                generatorName='resourceLaneBottomContent'
-                customGenerator={options.resourceLaneBottomContent}
-                classNameGenerator={options.resourceLaneBottomClass}
-              />
-            </div>
-          )}
-        />
+                  {eventBands.map((band) => (
+                    <TimelinePrintEventBand
+                      {...timelineBandProps}
+                      key={band.levelIndex}
+                      band={band}
+                      heightRefMap={this.printHeights.segHeightRefMap}
+                    />
+                  ))}
+                  {moreLinkBand && (
+                    <TimelinePrintMoreLinkBand
+                      {...timelineBandProps}
+                      band={moreLinkBand}
+                      heightRefMap={this.printHeights.moreLinkHeightRefMap}
+                    />
+                  )}
+                  <ContentContainer
+                    tag='div'
+                    className={joinClassNames(classNames.noMargin, classNames.noShrink)}
+                    renderProps={renderProps}
+                    generatorName='resourceLaneBottomContent'
+                    customGenerator={options.resourceLaneBottomContent}
+                    classNameGenerator={options.resourceLaneBottomClass}
+                  />
+                </>
+              )}
+            </ContentContainer>
+          </div>
+        </div>
       </div>
     )
   }
-}
-
-interface ResourcePrintSectionProps {
-  spreadsheetWidth: CssDimValue
-  dividerClassName: ClassNameInput
-  spreadsheetContent: ReactNode
-  timelineContent: ReactNode
-}
-
-/** Keeps one spreadsheet continuation and its Timeline band in one print fragment. */
-function ResourcePrintSection(props: ResourcePrintSectionProps) {
-  return (
-    <div className={joinClassNames(
-      classNames.flexRow,
-      classNames.breakInsideAvoid,
-      classNames.rel,
-      classNames.z1,
-    )}>
-      <div
-        className={joinClassNames(classNames.flexCol, classNames.crop)}
-        style={{ width: props.spreadsheetWidth }}
-      >
-        {props.spreadsheetContent}
-      </div>
-      <div className={joinClassNames(props.dividerClassName)} />
-      <div className={joinClassNames(classNames.flexCol, classNames.crop, classNames.liquid)}>
-        {props.timelineContent}
-      </div>
-    </div>
-  )
-}
-
-interface SpreadsheetPlaceholderCellsProps {
-  layout: ResourcePrintLayout
-  colGroupStats: ColGroupStats[]
-  colSpecs: ColSpec[]
-  groupColCnt: number
-  colWidths: number[] | undefined
-  colGrows: number[] | undefined
-  spreadsheetCanvasWidth: CssDimValue | undefined
-  resourceRowClassName: ClassNameInput
-  resourceBorderBottom: boolean
-  drawBottomBorders: boolean
-}
-
-/** Draws continuation borders without repeating spreadsheet content or cell semantics. */
-function SpreadsheetPlaceholderCells(props: SpreadsheetPlaceholderCellsProps) {
-  const colWidths = props.colWidths || []
-  const colGrows = props.colGrows || []
-  let resourceWidth: number | undefined
-  let resourceGrow: number | undefined
-
-  if (props.colWidths) {
-    resourceWidth = totalColDims(props.colWidths, props.groupColCnt, props.colSpecs.length)
-  }
-  if (props.colGrows) {
-    resourceGrow = totalColDims(props.colGrows, props.groupColCnt, props.colSpecs.length)
-  }
-
-  return (
-    <div
-      aria-hidden
-      className={joinClassNames(classNames.grow, classNames.flexRow)}
-      style={{ minWidth: props.spreadsheetCanvasWidth }}
-    >
-      {props.layout.colGroups.map((_colGroup, colIndex) => {
-        const drawBottomBorder = props.drawBottomBorders &&
-          props.colGroupStats[colIndex].borderBottom
-
-        return (
-          <div
-            key={colIndex} // eslint-disable-line react/no-array-index-key
-            className={joinClassNames(
-              props.resourceRowClassName,
-              classNames.flexRow,
-              drawBottomBorder ? classNames.borderOnlyB : classNames.borderNone,
-            )}
-            style={{
-              minWidth: 0,
-              width: colWidths[colIndex],
-              flexGrow: colGrows[colIndex],
-            }}
-          >
-            {/* A merged-group continuation row has no inner group cell, so its
-            sections must not draw one either */}
-            {props.colGroupStats[colIndex].render && (
-              <div className={joinClassNames(
-                props.resourceRowClassName,
-                classNames.noMargin,
-                classNames.noPadding,
-                classNames.flexCol,
-                classNames.liquid,
-                colIndex ? classNames.borderOnlyS : classNames.borderNone,
-              )} />
-            )}
-          </div>
-        )
-      })}
-      <div
-        className={joinClassNames(
-          props.resourceRowClassName,
-          classNames.flexRow,
-          props.resourceBorderBottom ? classNames.borderOnlyB : classNames.borderNone,
-        )}
-        style={{
-          width: resourceWidth,
-          flexGrow: resourceGrow,
-        }}
-      >
-        {mapRange(props.groupColCnt, props.colSpecs.length, (colIndex) => (
-          <div
-            key={colIndex} // eslint-disable-line react/no-array-index-key
-            className={joinClassNames(
-              props.resourceRowClassName,
-              classNames.noMargin,
-              classNames.noPadding,
-              classNames.flexCol,
-              classNames.alignStart,
-              (props.groupColCnt || colIndex) ? classNames.borderOnlyS : classNames.borderNone,
-              classNames.crop,
-            )}
-            style={{
-              minWidth: 0,
-              width: colWidths[colIndex],
-              flexGrow: colGrows[colIndex],
-            }}
-          />
-        ))}
-      </div>
-    </div>
-  )
-}
-
-function mapRange<Item>(start: number, end: number, func: (index: number) => Item): Item[] {
-  const items: Item[] = []
-
-  for (let i = start; i < end; i++) {
-    items.push(func(i))
-  }
-
-  return items
-}
-
-function totalColDims(colDims: number[], startIndex: number, endIndex: number): number {
-  let total = 0
-
-  for (let i = startIndex; i < endIndex; i++) {
-    total += colDims[i]
-  }
-
-  return total
 }
